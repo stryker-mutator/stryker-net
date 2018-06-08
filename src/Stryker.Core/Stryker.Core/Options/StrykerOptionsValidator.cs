1ee48595cc6495cdfb84001022a101217be7b53f
StefanKr <stefan.kraaikamp@infosupport.com>
2018-06-08 08:57:08 +0200

    refactor: rename
    Rename binairy to binary.

--- src/Stryker.Core/Stryker.Core/Options/StrykerOptionsValidator.cs
+++ src/Stryker.Core/Stryker.Core/Options/StrykerOptionsValidator.cs
@@ -65,7 +65,7 @@ namespace Stryker.Core.Options
                 mutators = new Collection<IMutator>
                 {
                     // the default list of mutators
-                    new BinairyExpressionMutator(),
+                    new BinaryExpressionMutator(),
                     new BooleanMutator(),
                 };
             }
