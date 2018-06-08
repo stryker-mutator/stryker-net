d857c5adde7d436835de664f20fa2a47d14bc951
StefanKr <stefan.kraaikamp@infosupport.com>
2018-06-08 09:30:28 +0200

    refactor: change
    Change the type of the new Mutation in the BinaryExpressionMutator

--- src/Stryker.Core/Stryker.Core/Mutators/BinaryExpressionMutator.cs
+++ src/Stryker.Core/Stryker.Core/Mutators/BinaryExpressionMutator.cs
@@ -44,7 +44,7 @@ namespace Stryker.Core.Mutators
                         OriginalNode = node,
                         ReplacementNode = replacementNode,
                         DisplayName = "Binary expression mutation",
-                        Type = "MathMutator"
+                        Type = "BinaryExpressionMutator"
                     };
                 }
             }
