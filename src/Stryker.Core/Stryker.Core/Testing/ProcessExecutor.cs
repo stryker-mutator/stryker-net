a4ff9ca03cca4c10426957834c5dcf0690d04815
Richard Werkman <Richard.Werkman@infosupport.com>
2018-06-18 10:32:41 +0200

    removed unnecessary nuget package reference

--- src/Stryker.Core/Stryker.Core/Testing/ProcessExecutor.cs
+++ src/Stryker.Core/Stryker.Core/Testing/ProcessExecutor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Management;
 using System.Threading;
 using System.Threading.Tasks;
 
