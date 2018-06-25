ae712cb6ef0638a0890eed0aa5a0616ed4cac8ff
manuc66 <manuc66@gmail.com>
2018-06-26 00:25:25 +0200

    feat: UnaryExpression mutator #35 add some UnaryExpression mutator

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutators/PrefixUnaryMutatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Stryker.Core.Mutators;
+using System.Linq;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.Mutators
+{
+    public class PrefixUnaryMutatorTests
+    {
+        [Theory]
+        [InlineData(SyntaxKind.UnaryMinusExpression, SyntaxKind.UnaryPlusExpression)]
+        [InlineData(SyntaxKind.UnaryPlusExpression, SyntaxKind.UnaryMinusExpression)]
+        public void ShouldMutate(SyntaxKind original, SyntaxKind expected)
+        {
+            var target = new PrefixUnaryMutator();
+            var originalNode = SyntaxFactory.PrefixUnaryExpression(original,
+                SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(1)));
+
+            var result = target.ApplyMutations(originalNode).ToList();
+
+            Assert.Single(result);
+            Assert.True(result.First().ReplacementNode.IsKind(expected));
+        }
+    }
+}
---
+++ src/Stryker.Core/Stryker.Core/Mutators/PrefixUnaryMutator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Stryker.Core.Mutants;
+
+namespace Stryker.Core.Mutators
+{
+    public class PrefixUnaryMutator : Mutator<PrefixUnaryExpressionSyntax>, IMutator
+    {
+        private readonly Dictionary<SyntaxKind, SyntaxKind> _unaryWithOpposite = new Dictionary<SyntaxKind, SyntaxKind>()
+        {
+            {SyntaxKind.UnaryMinusExpression, SyntaxKind.UnaryPlusExpression},
+            {SyntaxKind.UnaryPlusExpression, SyntaxKind.UnaryMinusExpression},
+            {SyntaxKind.PreIncrementExpression, SyntaxKind.PreDecrementExpression},
+            {SyntaxKind.PreDecrementExpression, SyntaxKind.PreIncrementExpression},
+        };
+
+        public override IEnumerable<Mutation> ApplyMutations(PrefixUnaryExpressionSyntax node)
+        {
+            var unaryKind = node.Kind();
+            if (_unaryWithOpposite.TryGetValue(unaryKind, out var oppositeKind))
+            {
+                yield return new Mutation
+                {
+                    OriginalNode = node,
+                    ReplacementNode = SyntaxFactory.PrefixUnaryExpression(oppositeKind, node.Operand),
+                    DisplayName = unaryKind + " to " + oppositeKind + " mutation",
+                    Type = nameof(PrefixUnaryMutator)
+                };
+            }
+            else if (node.IsKind(SyntaxKind.BitwiseNotExpression))
+            {
+                yield return new Mutation
+                {
+                    OriginalNode = node,
+                    ReplacementNode = node.Operand,
+                    DisplayName = "Bitwise Not to un-Bitwise",
+                    Type = nameof(PrefixUnaryMutator)
+                };
+            }
+            else if (node.IsKind(SyntaxKind.LogicalNotExpression))
+            {
+                yield return new Mutation
+                {
+                    OriginalNode = node,
+                    ReplacementNode = node.Operand,
+                    DisplayName = "Bitwise Not to un-Bitwise",
+                    Type = nameof(PrefixUnaryMutator)
+                };
+            }
+        }
+    }
+}
