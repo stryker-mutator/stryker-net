c42ab46381ef715c421aea3da77906fc42c6f5d0
Richard Werkman <s1084402@student.windesheim.nl>
2018-07-13 14:07:17 +0200

    initialtestprocesstest refactor

--- src/Stryker.Core/Stryker.Core.UnitTest/Initialisation/InitialTestProcessTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Initialisation/InitialTestProcessTests.cs
@@ -11,15 +11,20 @@ namespace Stryker.Core.UnitTest.Initialisation
 {
     public class InitialTestProcessTests
     {
+        public InitialTestProcess _target { get; set; }
+
+        public InitialTestProcessTests()
+        {
+            _target = new InitialTestProcess();
+        }
+
         [Fact]
         public void InitialTestProcess_ShouldThrowExceptionOnFail()
         {
             var testRunnerMock = new Mock<ITestRunner>(MockBehavior.Strict);
             testRunnerMock.Setup(x => x.RunAll(It.IsAny<int>())).Returns(new TestRunResult() { Success = false });
 
-            var target = new InitialTestProcess();
-
-            var exception = Assert.Throws<InitialTestRunFailedException>(() => target.InitialTest(testRunnerMock.Object));
+            var exception = Assert.Throws<InitialTestRunFailedException>(() => _target.InitialTest(testRunnerMock.Object));
         }
 
         [Fact]
@@ -28,11 +33,7 @@ namespace Stryker.Core.UnitTest.Initialisation
             var testRunnerMock = new Mock<ITestRunner>(MockBehavior.Strict);
             testRunnerMock.Setup(x => x.RunAll(It.IsAny<int>())).Returns(new TestRunResult() { Success = true });
 
-            var target = new InitialTestProcess();
-
-            var result = target.InitialTest(testRunnerMock.Object);
-
-            ;
+            var result = _target.InitialTest(testRunnerMock.Object);
         }
     }
 }
