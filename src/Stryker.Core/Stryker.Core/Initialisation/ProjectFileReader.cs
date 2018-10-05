cde146b0248abacf7463fd8e6f7bb6b61b212c00
Keith Lewis <kclewis0614@gmail.com>
2018-10-05 08:52:16 -0500

    style: make minor formatting changes
    add minor formatting changes to bring in line with MS standards

--- src/Stryker.Core/Stryker.Core/Initialisation/ProjectFileReader.cs
+++ src/Stryker.Core/Stryker.Core/Initialisation/ProjectFileReader.cs
@@ -10,12 +10,12 @@ namespace Stryker.Core.Initialisation
     {
         private ILogger _logger { get; set; }
 
-        public ProjectFileReader ()
+        public ProjectFileReader()
         {
             _logger = ApplicationLogging.LoggerFactory.CreateLogger<ProjectFileReader> ();
         }
 
-        public ProjectFile ReadProjectFile (XDocument projectFileContents, string projectUnderTestNameFilter)
+        public ProjectFile ReadProjectFile(XDocument projectFileContents, string projectUnderTestNameFilter)
         {
             _logger.LogDebug ("Reading the project file {0}", projectFileContents.ToString ());
 
@@ -26,11 +26,11 @@ namespace Stryker.Core.Initialisation
             return new ProjectFile ()
             {
                 ProjectReference = rererence,
-                    TargetFramework = targetFramework
+                TargetFramework = targetFramework
             };
         }
 
-        private string FindProjectReference (XDocument document, string projectUnderTestNameFilter)
+        private string FindProjectReference(XDocument document, string projectUnderTestNameFilter)
         {
             _logger.LogDebug ("Determining project under test with name filter {0}", projectUnderTestNameFilter);
 
@@ -44,19 +44,22 @@ namespace Stryker.Core.Initialisation
                 if (string.IsNullOrEmpty (projectUnderTestNameFilter))
                 {
                     throw new NotSupportedException ("Only one referenced project is supported, please add the --project-file=[projectname] argument to specify the project to mutate", innerException : new Exception ($"Found the following references: {referencesString}"));
-                } else
+                }
+                else
                 {
                     var searchResult = projectReferences.Where (x => x.ToLower ().Contains (projectUnderTestNameFilter.ToLower ())).ToList ();
                     if (!searchResult.Any ())
                     {
                         throw new ArgumentException ($"No project reference matched your --project-file={projectUnderTestNameFilter} argument to specify the project to mutate, was the name spelled correctly?", innerException : new Exception ($"Found the following references: {referencesString}"));
-                    } else if (searchResult.Count () > 1)
+                    }
+                    else if (searchResult.Count () > 1)
                     {
                         throw new ArgumentException ($"More than one project reference matched your --project-file={projectUnderTestNameFilter} argument to specify the project to mutate, please specify the name more detailed", innerException : new Exception ($"Found the following references: {referencesString}"));
                     }
                     return searchResult.Single ();
                 }
-            } else if (!projectReferences.Any ())
+            }
+            else if (!projectReferences.Any ())
             {
                 throw new NotSupportedException ("No project references found in test project file, unable to find project to mutate.");
             }
@@ -64,7 +67,7 @@ namespace Stryker.Core.Initialisation
 
         }
 
-        private string FindTargetFrameworkReference (XDocument document)
+        private string FindTargetFrameworkReference(XDocument document)
         {
             if (document.Elements().Descendants("TargetFrameworks").Any())
             {
@@ -76,7 +79,7 @@ namespace Stryker.Core.Initialisation
             }
         }
 
-        public string FindAssemblyName (XDocument document)
+        public string FindAssemblyName(XDocument document)
         {
             return document.Elements ().Descendants ().Where (x => string.Equals (x.Name.LocalName, "AssemblyName", StringComparison.OrdinalIgnoreCase)).FirstOrDefault ()?.Value;
         }
