964e04ee030e82f1db7ea2b9aab5a9027cd06200
Tisu <piotrszkudlarski@gmail.com>
2018-10-09 00:27:44 +0200

    Added test for ProgressReporter, add progress bar and changed time formating

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/Reporters/Progress/ProgressReporterTests.cs
@@ -0,0 +1,125 @@
+﻿using Moq;
+using Stryker.Core.Mutants;
+using Stryker.Core.Reporters.Progress;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.Reporters.Progress
+{
+    public class ProgressReporterTests
+    {
+        private readonly Mock<IConsoleOneLineLogger> _testsProgressLogger;
+        private readonly Mock<IConsoleOneLineLogger> _mutantKilledLogger;
+        private readonly Mock<IConsoleOneLineLogger> _mutantSurvivedLogger;
+        private readonly Mock<IConsoleOneLineLogger> _mutantTimeoutLogger;
+        private readonly Mock<IConsoleOneLineLogger> _mutantRuntimeErrorLogger;
+
+        private readonly ProgressReporter _progressReporter;
+
+        public ProgressReporterTests()
+        {
+            _testsProgressLogger = new Mock<IConsoleOneLineLogger>();
+            _mutantKilledLogger = new Mock<IConsoleOneLineLogger>();
+            _mutantSurvivedLogger = new Mock<IConsoleOneLineLogger>();
+            _mutantTimeoutLogger = new Mock<IConsoleOneLineLogger>();
+            _mutantRuntimeErrorLogger = new Mock<IConsoleOneLineLogger>();
+
+            _progressReporter = new ProgressReporter(_testsProgressLogger.Object,
+                                                     _mutantKilledLogger.Object,
+                                                     _mutantSurvivedLogger.Object,
+                                                     _mutantTimeoutLogger.Object,
+                                                     _mutantRuntimeErrorLogger.Object);
+        }
+
+        [Fact]
+        public void ProgressReporter_ShouldLogEachTimeKilledMutantIsReported()
+        {
+            var mutantTestResult = new Mutant()
+            {
+                ResultStatus = MutantStatus.Killed
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                _progressReporter.ReportRunTest(TimeSpan.MaxValue, mutantTestResult);
+                _mutantKilledLogger.Verify(x => x.ReplaceLog(It.IsAny<string>(), It.Is<object[]>(y => y.Length == 1 && (int)y.First() == i + 1)));
+            }
+        }
+
+        [Fact]
+        public void ProgressReporter_ShouldLogEachTimeSurvivedMutantIsReported()
+        {
+            var mutantTestResult = new Mutant()
+            {
+                ResultStatus = MutantStatus.Survived
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                _progressReporter.ReportRunTest(TimeSpan.MaxValue, mutantTestResult);
+                _mutantSurvivedLogger.Verify(x => x.ReplaceLog(It.IsAny<string>(), It.Is<object[]>(y => y.Length == 1 && (int)y.First() == i + 1)));
+            }
+        }
+
+        [Fact]
+        public void ProgressReporter_ShouldLogEachTimeTimeoutMutantIsReported()
+        {
+            var mutantTestResult = new Mutant()
+            {
+                ResultStatus = MutantStatus.Timeout
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                _progressReporter.ReportRunTest(TimeSpan.MaxValue, mutantTestResult);
+                _mutantTimeoutLogger.Verify(x => x.ReplaceLog(It.IsAny<string>(), It.Is<object[]>(y => y.Length == 1 && (int)y.First() == i + 1)));
+            }
+        }
+
+        [Fact]
+        public void ProgressReporter_ShouldLogEachTimeRuntimeErrorMutantIsReported()
+        {
+            var mutantTestResult = new Mutant()
+            {
+                ResultStatus = MutantStatus.RuntimeError
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                _progressReporter.ReportRunTest(TimeSpan.MaxValue, mutantTestResult);
+                _mutantRuntimeErrorLogger.Verify(x => x.ReplaceLog(It.IsAny<string>(), It.Is<object[]>(y => y.Length == 1 && (int)y.First() == i + 1)));
+            }
+        }
+
+        [Fact]
+        public void ProgressReporter_ShouldReportTestProgressAs100PercentageDone_WhenTotalNumberOfTestsIsZero()
+        {
+            _progressReporter.StartReporting(0);
+
+            _progressReporter.ReportRunTest(TimeSpan.FromSeconds(10), new Mutant());
+            _testsProgressLogger.Verify(x => x.ReplaceLog(It.IsAny<string>(),
+                                                          It.Is<object[]>(
+                                                              y => (int)y.ElementAt(1) == 0 &&
+                                                                   (int)y.ElementAt(2) == 0 &&
+                                                                   (int)y.ElementAt(3) == 100 &&
+                                                                   ((string)y.ElementAt(4)).Contains("0 s"))));
+        }
+
+        [Fact]
+        public void ProgressReporter_ShouldReportTestProgressAs50PercentageDone_And_FirstTestExecutionTime_WhenHalfOfTestsAreDone()
+        {
+            _progressReporter.StartReporting(2);
+
+            _progressReporter.ReportRunTest(TimeSpan.FromSeconds(10), new Mutant());
+
+            _testsProgressLogger.Verify(x => x.ReplaceLog(It.IsAny<string>(),
+                                                          It.Is<object[]>(
+                                                              y => ((string)y.ElementAt(0)).EndsWith("-----") &&
+                                                                   (int)y.ElementAt(1) == 1 &&
+                                                                   (int)y.ElementAt(2) == 2 &&
+                                                                   (int)y.ElementAt(3) == 50 &&
+                                                                   (string)y.ElementAt(4) == "10 s")));
+        }
+    }
+}
--- src/Stryker.Core/Stryker.Core/Reporters/Progress/ProgressReporter.cs
+++ src/Stryker.Core/Stryker.Core/Reporters/Progress/ProgressReporter.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Text;
 
 namespace Stryker.Core.Reporters.Progress
 {
@@ -13,6 +14,11 @@ namespace Stryker.Core.Reporters.Progress
 
     public class ProgressReporter : IProgressReporter
     {
+        private const int MaxProgressBar = 10;
+        private const char ProgressBarDoneToken = '\u2588';
+        private const char ProgressBarLeftToken = '-';
+        private const int ProgressBarInitialState = -1;
+
         private readonly IConsoleOneLineLogger _testsProgressLogger;
 
         private readonly IConsoleOneLineLogger _mutantKilledLogger;
@@ -21,7 +27,7 @@ namespace Stryker.Core.Reporters.Progress
         private readonly IConsoleOneLineLogger _mutantRuntimeErrorLogger;
 
         private readonly object _mutex = new object();
-        private readonly ConcurrentBag<double> _pastTestRunTimers = new ConcurrentBag<double>();
+        private readonly ConcurrentBag<TimeSpan> _pastTestRunTimers = new ConcurrentBag<TimeSpan>();
 
         private int _totalNumberOfTests;
         private int _numberOfTestsRun;
@@ -47,8 +53,16 @@ namespace Stryker.Core.Reporters.Progress
         public void StartReporting(int totalNumberOfTests)
         {
             _totalNumberOfTests = totalNumberOfTests;
+            var totalNumberOfTestsPercentage = _numberOfTestsRun * 100 / GetTotalNumberOfTestsNotZero();
 
-            _testsProgressLogger.StartLog("Tests progress | | {0} / {1} | {2} % | ~ {3}s |", _numberOfTestsRun, _totalNumberOfTests, _numberOfTestsRun * 100 / _totalNumberOfTests, 0);
+            var progressBar = GenerateProgressBar(ProgressBarInitialState);
+
+            _testsProgressLogger.StartLog("Tests progress | {0} | {1} / {2} | {3} % | ~ {4}s |",
+                                          progressBar,
+                                          CorrectNumberOfTestsRunIfTotalNumberOfTestsIsZero(),
+                                          _totalNumberOfTests,
+                                          totalNumberOfTestsPercentage,
+                                          "0");
 
             _mutantKilledLogger.StartLog("Killed : {0}", 0);
             _mutantSurvivedLogger.StartLog("Survived: {0}", 0);
@@ -59,18 +73,79 @@ namespace Stryker.Core.Reporters.Progress
 
         public void ReportRunTest(TimeSpan timerElapsed, IReadOnlyMutant mutantTestResult)
         {
-            _pastTestRunTimers.Add(timerElapsed.TotalSeconds);
+            _pastTestRunTimers.Add(timerElapsed);
             lock (_mutex)
             {
                 _numberOfTestsRun++;
                 ReportMutantTestResult(mutantTestResult);
-                var leftTests = _totalNumberOfTests - _numberOfTestsRun;
+                var testsLeft = _totalNumberOfTests - _numberOfTestsRun;
 
-                var secondsLeft = Math.Round(leftTests * _pastTestRunTimers.Average(), 2);
+                testsLeft = CorrectTestsLeftIfZero(testsLeft);
+
+                var totalNumberOfTestsPercentage = _numberOfTestsRun * 100 / GetTotalNumberOfTestsNotZero();
+                var secondsLeft = Math.Round(testsLeft * _pastTestRunTimers.Average(x => x.TotalSeconds), 2);
                 var timeLeft = TimeSpan.FromSeconds(secondsLeft);
+                var progressBarCount = totalNumberOfTestsPercentage / 10;
 
-                _testsProgressLogger.ReplaceLog("Tests progress | | {0} / {1} | {2} % | ~ {3}m{4}s |", _numberOfTestsRun, _totalNumberOfTests, _numberOfTestsRun * 100 / _totalNumberOfTests, timeLeft.Minutes, timeLeft.Seconds);
+                var stringBuilder = GenerateProgressBar(progressBarCount);
+
+                var formattedTime = string.Format(GetTimeFormat(timeLeft), timeLeft);
+                _testsProgressLogger.ReplaceLog("Tests progress | {0} | {1} / {2} | {3} % | ~ {4} |",
+                                                stringBuilder, CorrectNumberOfTestsRunIfTotalNumberOfTestsIsZero(), _totalNumberOfTests,
+                                                totalNumberOfTestsPercentage, formattedTime);
             }
+        }
+
+        private int CorrectNumberOfTestsRunIfTotalNumberOfTestsIsZero()
+        {
+            return _totalNumberOfTests == 0 ? 0 :_numberOfTestsRun;
+        }
+
+        private static int CorrectTestsLeftIfZero(int testsLeft)
+        {
+            return testsLeft < 0 ? 0 : testsLeft;
+        }
+
+        private int GetTotalNumberOfTestsNotZero()
+        {
+            return _totalNumberOfTests == 0 ? 1 : _totalNumberOfTests;
+        }
+
+        private static string GetTimeFormat(TimeSpan timeLeft)
+        {
+            if (timeLeft.Days > 0)
+            {
+                return "{0:%d} days {0:%h} h {0:%m} m {0:%s} s";
+            }
+
+            if (timeLeft.Hours > 0)
+            {
+                return "{0:%h} h {0:%m} m {0:%s} s";
+            }
+
+            if (timeLeft.Minutes > 0)
+            {
+                return "{0:%m} m {0:%s} s";
+            }
+
+            return "{0:%s} s";
+        }
+
+        private string GenerateProgressBar(int progressBarCount)
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = 0; i < MaxProgressBar; i++)
+            {
+                if (i < progressBarCount)
+                {
+                    stringBuilder.Append(ProgressBarDoneToken);
+                    continue;
+                }
+
+                stringBuilder.Append(ProgressBarLeftToken);
+            }
+
+            return stringBuilder.ToString();
         }
 
         private void ReportMutantTestResult(IReadOnlyMutant mutantTestResult)
