85c48220fdc0c97c191bbb883ae5807ecbc3d58f
Tisu <piotrszkudlarski@gmail.com>
2018-10-09 19:48:35 +0200

    Correct wrong line number in mutant reports

--- src/Stryker.Core/Stryker.Core/Reporters/ConsoleReportReporter.cs
+++ src/Stryker.Core/Stryker.Core/Reporters/ConsoleReportReporter.cs
@@ -61,7 +61,7 @@ namespace Stryker.Core.Reporters
                     {
                         _chalk.Red($"[{mutant.ResultStatus}] ");
                     }
-                    _chalk.Default($"{mutant.Mutation.DisplayName} on line {mutant.Mutation.OriginalNode.GetLocation().GetLineSpan().StartLinePosition.Line}: '{mutant.Mutation.OriginalNode}' ==> '{mutant.Mutation.ReplacementNode}'{Environment.NewLine}");
+                    _chalk.Default($"{mutant.Mutation.DisplayName} on line {mutant.Mutation.OriginalNode.GetLocation().GetLineSpan().StartLinePosition.Line + 1}: '{mutant.Mutation.OriginalNode}' ==> '{mutant.Mutation.ReplacementNode}'{Environment.NewLine}");
                 }
             };
             
