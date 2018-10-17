2b2f9baec52b98447f813c8a3e4a146d0d8246e3
Richard Werkman <Richard1158@gmail.com>
2018-10-17 21:48:11 +0200

    feat(testing): Add happyflow integration test (#163)
    * Initial try for an integration test
    
    * Moved to a new solution for integrationtests
    
    * Remove old project reference
    
    * Change integrationtest to run CLI instread of Core project
    
    * Make integration test succeed
    
    * Deleted unneeded files for integration test
    
    * Add loglevel trace to integrationtest to see what takes longest on azure pipelines

---
+++ integrationtests/IntegrationTests/Stryker.Core.IntegrationTest/ExampleProject/ExampleProject/DummyMath.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleProject
+{
+    public class DummyMath
+    {
+        public int Add(int first, int second)
+        {
+            return checked(first + second);
+        }
+    }
+}
