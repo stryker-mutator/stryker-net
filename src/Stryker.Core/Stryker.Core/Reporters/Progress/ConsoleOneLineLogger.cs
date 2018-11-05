4ce82089b9a8ae34c693cfa343cb2f731c6164b5
Rouke Broersma <Mobrockers@users.noreply.github.com>
2018-11-05 20:07:23 +0100

    chore(progress reporter): Fix crash on console cursor access on systems that do not have a console available (#229)

--- src/Stryker.Core/Stryker.Core/Reporters/Progress/ConsoleOneLineLogger.cs
+++ src/Stryker.Core/Stryker.Core/Reporters/Progress/ConsoleOneLineLogger.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Stryker.Core.Reporters.Progress
@@ -11,6 +11,7 @@ namespace Stryker.Core.Reporters.Progress
 
     public class ConsoleOneLineLogger : IConsoleOneLineLogger
     {
+        private bool _hasConsole;
         private int _cursorTop;
 
         private readonly ILogger _logger;
@@ -18,31 +19,57 @@ namespace Stryker.Core.Reporters.Progress
         public ConsoleOneLineLogger(ILogger logger)
         {
             _logger = logger;
+            _hasConsole = EnvironmentHasConsole();
         }
 
         public void StartLog(string text, params object[] args)
         {
-            _cursorTop = Console.CursorTop;
+            if (_hasConsole)
+            {
+                _cursorTop = Console.CursorTop;
+            }
             _logger.LogInformation(text, args);
         }
 
         public void ReplaceLog(string text, params object[] args)
         {
-            var currentCursorTop = Console.CursorTop;
-            var currentCursorLeft = Console.CursorLeft;
+            int currentCursorTop = 0;
+            int currentCursorLeft = 0;
+            if (_hasConsole)
+            {
+                currentCursorTop = Console.CursorTop;
+                currentCursorLeft = Console.CursorLeft;
 
-            Console.SetCursorPosition(0, _cursorTop);
-            ClearLine();
+                Console.SetCursorPosition(0, _cursorTop);
+                ClearLine();
 
-            Console.SetCursorPosition(0, _cursorTop);
+                Console.SetCursorPosition(0, _cursorTop);
+            }
+
             _logger.LogInformation(text, args);
 
-            Console.SetCursorPosition(currentCursorLeft, currentCursorTop);
+            if (_hasConsole)
+            {
+                Console.SetCursorPosition(currentCursorLeft, currentCursorTop);
+            }
         }
 
         private void ClearLine()
         {
             _logger.LogInformation(new string(' ', Console.WindowWidth));
+        }
+
+        private bool EnvironmentHasConsole()
+        {
+            try
+            {
+                var t = Console.CursorTop;
+                return true;
+            }
+            catch (System.IO.IOException)
+            {
+                return false;
+            }
         }
     }
 }
