c8b52e0ceea3ffcaacf1087cb6017c1d5f06c979
Rouke Broersma <rouke.broersma@infosupport.com>
2018-11-17 17:37:18 +0100

    Manually update DotNetCliToolReference in csproj

---
+++ prepare-integrationtest.js
@@ -0,0 +1,21 @@
+var newVersionNumber = process.argv.slice(2);
+
+const replaceVersionNumber = (path, oldString, newString) => {
+    const fileContent = fs.readFileSync(path, { encoding: 'UTF-8' });
+    if (!fileContent.includes(oldString)) {
+        throw new Error(`The file at ${path} did not contain ${oldString}!`);
+    }
+    const oldStringStart = fileContent.indexOf(oldString);
+    const oldStringEnd = oldStringStart + oldString.length;
+    const updatedFileContent = fileContent.substr(0, oldStringStart) + newString + fileContent.substr(oldStringEnd);
+    fs.writeFileSync(path, updatedFileContent, { encoding: 'UTF-8' });
+};
+
+const packages = [
+    { csproj: './integrationtests/IntegrationTests/ExampleProject.XUnit/ExampleProject.XUnit.csproj' },
+];
+
+packages.forEach(package => {
+    console.log(`Updating DotNetCliToolReference version number in ${package.csproj}`);
+    replaceVersionNumber(package.csproj, `<DotNetCliToolReference Include="StrykerMutator.DotNetCoreCli" Version="*" />`, `<DotNetCliToolReference Include="StrykerMutator.DotNetCoreCli" Version="${newVersionNumber}" />`);
+});
