bd76dd19b88710812dd879fe8d18383d24c083f3
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2018-11-27 18:14:18 +0100

    Fix failing tests

--- src/Stryker.Core/Stryker.Core.UnitTest/Mutants/TestResources/Mutator_SyntaxShouldBe_ConditionalStatement_OUT.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutants/TestResources/Mutator_SyntaxShouldBe_ConditionalStatement_OUT.cs
@@ -9,9 +9,9 @@ namespace StrykerNet.UnitTest.Mutants.TestResources
         void TestMethod()
         {
             int test = 10;
-            int testPlusTest = Stryker.Helper.ActiveMutation==0 ? test - test : test + test;
-            int b = Stryker.Helper.ActiveMutation==2 ? a += 1 - 2 : Stryker.Helper.ActiveMutation==1 ? a -= 1 + 2 : a += 1 + 2;
-            int Add(int x, int y) => Stryker.Helper.ActiveMutation==3 ? x - y : x + y;
+            int testPlusTest = Stryker.ActiveMutationHelper.ActiveMutation==0 ? test - test : test + test;
+            int b = Stryker.ActiveMutationHelper.ActiveMutation==2 ? a += 1 - 2 : Stryker.ActiveMutationHelper.ActiveMutation==1 ? a -= 1 + 2 : a += 1 + 2;
+            int Add(int x, int y) => Stryker.ActiveMutationHelper.ActiveMutation==3 ? x - y : x + y;
         }
     }
 }
