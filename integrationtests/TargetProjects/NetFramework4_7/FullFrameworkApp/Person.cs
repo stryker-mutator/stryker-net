c8a3d854f19c8f54ca66261294f8a76bb03c78c7
Richard Werkman <Richard.Werkman@infosupport.com>
2019-03-01 16:21:54 +0100

    Add integration test project for full framework

---
+++ integrationtests/TargetProjects/NetFramework4_7/FullFrameworkApp/Person.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FullFrameworkApp
+{
+    public class Person
+    {
+        public int Age { get; set; }
+
+        public void Aged()
+        {
+            if (Age > 0)
+            {
+                Age++;
+            }
+        }
+    }
+}
