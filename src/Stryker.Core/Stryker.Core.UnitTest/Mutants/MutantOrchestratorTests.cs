d759d75c80962fdeccad08d24fbaeedeb875454b
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2019-03-05 14:29:30 +0100

    Tests(MutantOrchestrator): asserts the location of some mutants to prevent some regressions

--- src/Stryker.Core/Stryker.Core.UnitTest/Mutants/MutantOrchestratorTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutants/MutantOrchestratorTests.cs
@@ -42,9 +42,10 @@ namespace Stryker.Core.UnitTest.Mutants
         }
 
         [Theory]
-        [InlineData("Mutator_FromActualProject_IN.cs", "Mutator_FromActualProject_OUT.cs")]
-        [InlineData("Mutator_KnownComplexCases_IN.cs", "Mutator_KnownComplexCases_OUT.cs")]
-        public void Mutator_TestResourcesInputShouldBecomeOutputForFullScope(string inputFile, string outputFile)
+        [InlineData("Mutator_FromActualProject_IN.cs", "Mutator_FromActualProject_OUT.cs", 24, 5, 14, 12, 30)]
+        [InlineData("Mutator_KnownComplexCases_IN.cs", "Mutator_KnownComplexCases_OUT.cs", 10, 2, 16, 6, 21)]
+        public void Mutator_TestResourcesInputShouldBecomeOutputForFullScope(string inputFile, string outputFile, 
+            int nbMutants, int mutant1Id, int mutant1Location, int mutant2Id, int mutant2Location)
         {
             string source = File.ReadAllText(_currentDirectory + "/Mutants/TestResources/" + inputFile);
             string expected = File.ReadAllText(_currentDirectory + "/Mutants/TestResources/" + outputFile);
@@ -52,6 +53,11 @@ namespace Stryker.Core.UnitTest.Mutants
             var actualNode = target.Mutate(CSharpSyntaxTree.ParseText(source).GetRoot());
             var expectedNode = CSharpSyntaxTree.ParseText(expected).GetRoot();
             actualNode.ShouldBeSemantically(expectedNode);
+
+            var mutants = target.GetLatestMutantBatch().ToList();
+            mutants.Count.ShouldBe(nbMutants);
+            mutants[mutant1Id].Mutation.OriginalNode.GetLocation().GetLineSpan().StartLinePosition.Line.ShouldBe(mutant1Location);
+            mutants[mutant2Id].Mutation.OriginalNode.GetLocation().GetLineSpan().StartLinePosition.Line.ShouldBe(mutant2Location);
         }
 
         [Theory]
