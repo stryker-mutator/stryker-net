45b696aab54b9a0ad9f1b03cdde84477ece5b89c
Rouke Broersma <mobrockers@gmail.com>
2019-03-08 20:31:32 +0100

    Test

--- integrationtest/ValidationProject/ValidateStrykerResults.cs
+++ integrationtest/ValidationProject/ValidateStrykerResults.cs
@@ -2,7 +2,6 @@ using Newtonsoft.Json;
 using Shouldly;
 using Stryker.Core.Mutants;
 using Stryker.Core.Reporters.Json;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Xunit;
@@ -12,9 +11,9 @@ namespace IntegrationTests
     public class ValidateStrykerResults
     {
         [Fact]
-        public void NetCore2_0()
+        public void NetCore2_1()
         {
-            var directory = new DirectoryInfo("../TargetProjects/TestProject.XUnit/StrykerOutput/reports");
+            var directory = new DirectoryInfo("../TargetProjects/NetCoreTestProject.XUnit/StrykerOutput/reports");
             directory.GetFiles("*.json").ShouldNotBeNull("No reports available to assert");
 
             var latestReport = directory.GetFiles("*.json")
@@ -25,13 +24,13 @@ namespace IntegrationTests
 
             var report = JsonConvert.DeserializeObject<JsonReport>(strykerRunOutput);
 
-            CheckReportMutantCounts(report, total: 15, skipped: 1, survived: 12, killed: 2, timeout: 1);
+            CheckReportMutantCounts(report, total: 23, skipped: 0, survived: 20, killed: 2, timeout: 1);
         }
 
         [Fact]
-        public void NetCore2_1()
+        public void NetStandard2_0()
         {
-            var directory = new DirectoryInfo("../TargetProjects/TestProject.XUnit/StrykerOutput/reports");
+            var directory = new DirectoryInfo("../TargetProjects/NetStandardTestProject.XUnit/StrykerOutput/reports");
             directory.GetFiles("*.json").ShouldNotBeNull("No reports available to assert");
 
             var latestReport = directory.GetFiles("*.json")
@@ -42,7 +41,7 @@ namespace IntegrationTests
 
             var report = JsonConvert.DeserializeObject<JsonReport>(strykerRunOutput);
 
-            CheckReportMutantCounts(report, total: 15, skipped: 1, survived: 12, killed: 2, timeout: 1);
+            CheckReportMutantCounts(report, total: 15, skipped: 8, survived: 12, killed: 2, timeout: 1);
         }
 
         private void CheckReportMutantCounts(JsonReport report, int total, int skipped, int survived, int killed, int timeout)
@@ -52,37 +51,6 @@ namespace IntegrationTests
             report.Files.Select(f => f.Value.Mutants.Count(m => m.Status == MutantStatus.Survived.ToString())).Sum().ShouldBe(survived);
             report.Files.Select(f => f.Value.Mutants.Count(m => m.Status == MutantStatus.Killed.ToString())).Sum().ShouldBe(killed);
             report.Files.Select(f => f.Value.Mutants.Count(m => m.Status == MutantStatus.Timeout.ToString())).Sum().ShouldBe(timeout);
-        }
-
-        private void CheckMutantSpans(JsonReport report)
-        {
-
-
-
-        }
-
-        private IEnumerable<JsonMutant> GenerateJsonMutants()
-        {
-            var mutants = new List<JsonMutant>
-            {
-                new JsonMutant
-                {
-                    Id = 0,
-                    Location = new JsonMutantLocation(
-                        new JsonMutantPosition
-                        {
-                            Column = 0,
-                            Line = 0
-                        },
-                        new JsonMutantPosition
-                        {
-                            Column = 0,
-                            Line = 0
-                        })
-                }
-            };
-
-            return mutants;
         }
     }
 }
