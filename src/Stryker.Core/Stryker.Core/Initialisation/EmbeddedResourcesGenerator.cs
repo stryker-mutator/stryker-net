4002b500b982e7a0ad037c71a14111f2948ae265
Rouke Broersma <mobrockers@gmail.com>
2019-03-10 01:01:29 +0100

    Give mono.cecil a better chance to read the original dll before mutating

--- src/Stryker.Core/Stryker.Core/Initialisation/EmbeddedResourcesGenerator.cs
+++ src/Stryker.Core/Stryker.Core/Initialisation/EmbeddedResourcesGenerator.cs
@@ -11,11 +11,26 @@ namespace Stryker.Core.Initialisation
     {
         public static IEnumerable<ResourceDescription> GetManifestResources(string assemblyPath, ILogger logger)
         {
-            ModuleDefinition module;
-
             try
             {
-                module = ModuleDefinition.ReadModule(assemblyPath);
+                var resourceDescriptions = new List<ResourceDescription>();
+                using (ModuleDefinition module = ModuleDefinition.ReadModule(
+                    assemblyPath,
+                    new ReaderParameters(ReadingMode.Immediate)
+                    {
+                        InMemory = true,
+                        ReadWrite = false
+                    }))
+                {
+                    foreach (EmbeddedResource moduleResource in module.Resources.Where(r => r.ResourceType == ResourceType.Embedded))
+                    {
+                        resourceDescriptions.Add(
+                            new ResourceDescription(
+                                moduleResource.Name,
+                                () => moduleResource.GetResourceStream(),
+                                moduleResource.IsPublic));
+                    }
+                }
             }
             catch (Exception e)
             {
@@ -25,15 +40,6 @@ namespace Stryker.Core.Initialisation
 
                 yield break;
             }
-
-            foreach (EmbeddedResource moduleResource in module.Resources.Where(r => r.ResourceType == ResourceType.Embedded))
-            {
-                yield return new ResourceDescription(
-                    moduleResource.Name,
-                    () => moduleResource.GetResourceStream(),
-                    moduleResource.IsPublic);
-            }
-            module.Dispose();
         }
     }
 }
