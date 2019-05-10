935ad654dd05083322840db1f73e817cdabbae49
Rouke Broersma <mobrockers@gmail.com>
2019-05-10 14:38:18 +0200

    Use new reporter names in default values

--- src/Stryker.Core/Stryker.Core/Options/StrykerOptions.cs
+++ src/Stryker.Core/Stryker.Core/Options/StrykerOptions.cs
@@ -88,7 +88,7 @@ namespace Stryker.Core.Options
         {
             if (reporters == null)
             {
-                foreach (var reporter in new[] { Reporter.ConsoleProgressBar, Reporter.ConsoleReport })
+                foreach (var reporter in new[] { Reporter.Progress, Reporter.ClearText })
                 {
                     yield return reporter;
                 }
