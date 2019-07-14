1c595467561b595033da9ef141e9ab4cfcffd60e
Rouke Broersma <mobrockers@gmail.com>
2019-07-14 12:46:40 +0200

    Actually use default value

--- src/Stryker.CLI/Stryker.CLI/CLIOptions.cs
+++ src/Stryker.CLI/Stryker.CLI/CLIOptions.cs
@@ -87,12 +87,13 @@ namespace Stryker.CLI
             ArgumentDescription = @"Used for matching the project references when finding the project to mutate. Example: ""ExampleProject.csproj""",
             JsonKey = "project-file"
         };
-       
+
         public static readonly CLIOption<string> TestProjectFileName = new CLIOption<string>
         {
             ArgumentName = "--test-project-file",
             ArgumentShortName = "-tp <testProjectFileName>",
             ArgumentDescription = @"Used for specifying the test project if there are multiple projects in the folder. Example: ""ExampleTestProject.csproj""",
+            DefaultValue = _defaultOptions.TestProjectNameFilter,
             JsonKey = "test-project-file"
         };
 
