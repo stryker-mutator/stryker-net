56e3f8f0f8eeca1db59a6cf62d45956eee0c5765
Richard Werkman <Richard.Werkman@infosupport.com>
2019-08-28 22:00:51 +0200

    Forgot this file

--- src/Stryker.Core/Stryker.Core/MutantFilters/GitDiffMutantFilter.cs
+++ src/Stryker.Core/Stryker.Core/MutantFilters/GitDiffMutantFilter.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using LibGit2Sharp;
 using Stryker.Core.Mutants;
 using Stryker.Core.Options;
@@ -11,15 +14,24 @@ namespace Stryker.Core.MutantFilters
     public class GitDiffMutantFilter : IMutantFilter
     {
         public string DisplayName => throw new NotImplementedException();
+        private ICollection<string> _changedFiles = new Collection<string>();
 
-        public IEnumerable<Mutant> FilterMutants(IEnumerable<Mutant> mutants, FileLeaf file, StrykerOptions options)
+        public GitDiffMutantFilter()
         {
             using (var repo = new Repository(@"C:\Dev\Repos\stryker-net"))
             {
-                foreach (TreeEntryChanges c in repo.Diff.Compare<TreeChanges>())
+                foreach (var treeChanges in repo.Diff.Compare<TreeChanges>())
                 {
-                    Console.WriteLine(c);
+                    _changedFiles.Add(treeChanges.OldPath);
                 }
+            }
+        }
+
+        public IEnumerable<Mutant> FilterMutants(IEnumerable<Mutant> mutants, FileLeaf file, StrykerOptions options)
+        {
+            if (_changedFiles.Contains(file.FullPath))
+            {
+                return Enumerable.Empty<Mutant>();
             }
             return mutants;
         }
