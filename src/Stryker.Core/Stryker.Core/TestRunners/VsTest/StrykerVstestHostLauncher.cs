7dcef99790633bb8cc9a7b5f1444ff10128bd4e5
Rouke Broersma <mobrockers@gmail.com>
2019-12-16 17:58:06 +0100

    Replace env var in case it exists

--- src/Stryker.Core/Stryker.Core/TestRunners/VsTest/StrykerVstestHostLauncher.cs
+++ src/Stryker.Core/Stryker.Core/TestRunners/VsTest/StrykerVstestHostLauncher.cs
@@ -31,7 +31,7 @@ namespace Stryker.Core.TestRunners.VsTest
         {
             // https://github.com/stryker-mutator/stryker-net/issues/778
             // This enables vstest.console.dll to also roll forward on no candidate fx
-            envVars.Add("DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX", "2");
+            envVars["DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX"] = "2";
 
             _envVars = envVars;
             _id = id;
