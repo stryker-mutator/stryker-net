3ba4578f5784797708b9a976e04f1e96e824de51
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2020-01-17 07:07:06 +0100

    fix: add missing using for StrykrtInputException

--- src/Stryker.Core/Stryker.Core/Initialisation/ProjectInfo.cs
+++ src/Stryker.Core/Stryker.Core/Initialisation/ProjectInfo.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Stryker.Core.Exceptions;
 
 namespace Stryker.Core.Initialisation
 {
