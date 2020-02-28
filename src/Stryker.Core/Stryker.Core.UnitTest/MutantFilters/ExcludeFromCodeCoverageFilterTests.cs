47ba7fc3c9350e20736c0bf682688ae944d44fdc
Manuel Riezebosch <mriezebosch@gmail.com>
2020-02-28 09:48:32 +0100

    feat(filter): filter mutants with the exclude from code coverage attribute

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/MutantFilters/ExcludeFromCodeCoverageFilterTests.cs
@@ -0,0 +1,146 @@
+using System;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using Shouldly;
+using Stryker.Core.MutantFilters;
+using Stryker.Core.Mutants;
+using Stryker.Core.Options;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.MutantFilters
+{
+    public static class ExcludeFromCodeCoverageFilterTests
+    {
+        [Fact]
+        public static void OnMethod()
+        {
+            // Arrange
+            var mutant = Create(@"
+public class IgnoredMethodMutantFilter_NestedMethodCalls
+{
+    [ExcludeFromCodeCoverage]
+    private void TestMethod()
+    {
+        var t = Enumerable.Range(0, 9).Where(x => x < 5).ToList();
+    }
+}", "<");
+
+            var sut = new ExcludeFromCodeCoverageFilter() as IMutantFilter;
+
+            // Act
+            var results = sut.FilterMutants(new[] {mutant}, null, new StrykerOptions());
+
+            // Assert
+            results.ShouldNotContain(mutant);
+        }
+
+        [Fact]
+        public static void OnProperty()
+        {
+            // Arrange
+            var mutant = Create(@"
+public class IgnoredMethodMutantFilter_NestedMethodCalls
+{
+    [ExcludeFromCodeCoverage]
+    private string TestProperty => ""something""
+}", "something");
+
+            var sut = new ExcludeFromCodeCoverageFilter() as IMutantFilter;
+
+            // Act
+            var results = sut.FilterMutants(new[] {mutant}, null, new StrykerOptions());
+
+            // Assert
+            results.ShouldNotContain(mutant);
+        }
+        
+        [Fact]
+        public static void OnClass()
+        {
+            // Arrange
+            var mutant = Create(@"
+[ExcludeFromCodeCoverage]
+public class IgnoredMethodMutantFilter_NestedMethodCalls
+{
+    private void TestMethod()
+    {
+        var t = Enumerable.Range(0, 9).Where(x => x < 5).ToList();
+    }
+}", "<");
+
+            var sut = new ExcludeFromCodeCoverageFilter();
+
+            // Act
+            var results = sut.FilterMutants(new[] {mutant}, null, new StrykerOptions());
+
+            // Assert
+            results.ShouldNotContain(mutant);
+        }
+        
+        [Fact]
+        public static void Not()
+        {
+            // Arrange
+            var mutant = Create(@"
+public class IgnoredMethodMutantFilter_NestedMethodCalls
+{
+    private void TestMethod()
+    {
+        var t = Enumerable.Range(0, 9).Where(x => x < 5).ToList();
+    }
+}", "<");
+
+            var sut = new ExcludeFromCodeCoverageFilter();
+
+            // Act
+            var results = sut.FilterMutants(new[] {mutant}, null, new StrykerOptions());
+
+            // Assert
+            results.ShouldContain(mutant);
+        }
+        
+        [Theory]
+        [InlineData("System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAttribute")]
+        [InlineData("ExcludeFromCodeCoverageAttribute")]
+        [InlineData("ExcludeFromCodeCoverage")]
+        public static void Writings(string attr)
+        {
+            // Arrange
+            var mutant = Create($@"
+public class IgnoredMethodMutantFilter_NestedMethodCalls
+{{
+    [{attr}]
+    private void TestMethod()
+    {{
+        var t = Enumerable.Range(0, 9).Where(x => x < 5).ToList();
+    }}
+}}", "<");
+
+            var sut = new ExcludeFromCodeCoverageFilter();
+
+            // Act
+            var results = sut.FilterMutants(new[] {mutant}, null, new StrykerOptions());
+
+            // Assert
+            results.ShouldNotContain(mutant);
+        }
+        
+        
+        private static Mutant Create(string source, string search)
+        {
+            var baseSyntaxTree = CSharpSyntaxTree.ParseText(source).GetRoot();
+            var originalNode =
+                baseSyntaxTree.FindNode(new TextSpan(source.IndexOf(search, StringComparison.OrdinalIgnoreCase),
+                    5));
+
+            var mutant = new Mutant
+            {
+                Mutation = new Mutation
+                {
+                    OriginalNode = originalNode,
+                }
+            };
+            return mutant;
+        }
+    }
+}
