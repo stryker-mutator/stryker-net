03de920a398f41f2c44c334270ed10d11c58c216
Rouke Broersma <mobrockers@gmail.com>
2020-03-06 23:05:59 +0100

    Use correct URL and use Nuget.Versioning for version comparison

--- src/Stryker.CLI/Stryker.CLI/StrykerNugetFeedInfo.cs
+++ src/Stryker.CLI/Stryker.CLI/StrykerNugetFeedInfo.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+using NuGet.Versioning;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,21 +9,26 @@ namespace Stryker.CLI.NuGet
 {
     public class StrykerNugetFeedInfo
     {
-        [JsonProperty("items")]
-        private readonly List<NuGetLibraryItem> _items = null;
+        [JsonProperty("versions")]
+        private readonly List<string> _versions;
 
-        public string LatestVersion { get; private set; }
+        public SemanticVersion LatestVersion
+        {
+            get
+            {
+                return _versions.Max(v => SemanticVersion.Parse(v));
+            }
+        }
 
+        private StrykerNugetFeedInfo() { }
         public static async Task<StrykerNugetFeedInfo> Create()
         {
             using (var httpclient = new HttpClient())
             {
                 try
                 {
-                    var json = await httpclient.GetStringAsync("https://api.nuget.org/v3/registration3/dotnet-stryker/index.json");
-                    var instance = JsonConvert.DeserializeObject<StrykerNugetFeedInfo>(json, Converter.Settings);
-                    instance.LatestVersion = instance._items.First().Upper;
-
+                    var json = await httpclient.GetStringAsync("https://api.nuget.org/v3-flatcontainer/dotnet-stryker/index.json");
+                    var instance = JsonConvert.DeserializeObject<StrykerNugetFeedInfo>(json);
                     return instance;
                 }
                 catch
@@ -32,25 +36,6 @@ namespace Stryker.CLI.NuGet
                     return null;
                 }
             }
-        }
-
-        public class NuGetLibraryItem
-        {
-            [JsonProperty("upper")]
-            public string Upper { get; set; }
-        }
-
-        internal static class Converter
-        {
-            public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-            {
-                MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-                DateParseHandling = DateParseHandling.None,
-                Converters =
-            {
-                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
-            },
-            };
         }
     }
 }
