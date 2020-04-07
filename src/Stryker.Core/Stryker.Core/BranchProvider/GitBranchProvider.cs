8b05cf9cad370c7eb5012e665c2ef633e3d109b6
Stijn Rutten <Stijn.Rutten@infosupport.com>
2020-04-07 16:00:40 +0200

    Added branch provider

---
+++ src/Stryker.Core/Stryker.Core/BranchProvider/GitBranchProvider.cs
@@ -0,0 +1,44 @@
+﻿using LibGit2Sharp;
+using Stryker.Core.Exceptions;
+using Stryker.Core.Options;
+using Stryker.Core.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stryker.Core.BranchProvider
+{
+    public class GitBranchProvider : IBranchProvider
+    {
+        private readonly StrykerOptions _options;
+        private readonly Chalk _chalk;
+        public GitBranchProvider(StrykerOptions options)
+        {
+            _options = options;
+            _chalk = new Chalk();
+        }
+
+        public void GetBranchSHA()
+        {
+            string repositoryPath = Repository.Discover(_options.BasePath)?.Split(".git")[0];
+
+            if (string.IsNullOrEmpty(repositoryPath))
+            {
+                throw new StrykerInputException("Could not locate git repository. Unable to determine git diff to filter mutants. Did you run inside a git repo? If not please disable the --diff feature.");
+            }
+
+            using (var repo = new Repository(repositoryPath))
+            {
+                var currentBranchName = repo.Head.FriendlyName;
+                var currentBranch = repo.Branches[currentBranchName];
+
+                string currentBranchHash;
+                if(currentBranch.IsRemote)
+                {
+                    _chalk.Green(true.ToString());
+                }
+                
+            }
+        }
+    }
+}
---
+++ src/Stryker.Core/Stryker.Core/BranchProvider/IBranchProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stryker.Core.BranchProvider
+{
+    public interface IBranchProvider
+    {
+        void GetBranchSHA();
+    }
+}
