863b56a09f0407e490d25efcdecce60e0187861e
Richard Werkman <Richard1158@gmail.com>
2020-04-23 08:38:06 +0200

    feat(Collection initializer mutator): Add mutator for collections and arrays (#1023)
    * Add mutator for collections and implicit arrays
    
    * Add documentation
    
    * Remove impilicit array mutation as it was harder than I imagined
    
    * Update Mutators.md
    
    * Remove unnecessary using
    
    Co-authored-by: Richard Werkman <Richard.Werkman@infosupport.com>
    Co-authored-by: Rouke Broersma <Mobrockers@users.noreply.github.com>

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutators/InitializerMutatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Shouldly;
+using Stryker.Core.Mutators;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.Mutators
+{
+    public class InitializerMutatorTests
+    {
+        [Fact]
+        public void ShouldRemoveValuesFromArrayInitializer()
+        {
+            var initializerExpression = SyntaxFactory.InitializerExpression(SyntaxKind.ArrayInitializerExpression,
+                SyntaxFactory.SeparatedList(new List<ExpressionSyntax> {
+                    SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(5))
+                }));
+            var target = new InitializerMutator();
+
+            var result = target.ApplyMutations(initializerExpression);
+
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.DisplayName.ShouldBe("Array initializer mutation");
+
+            var replacement = mutation.ReplacementNode.ShouldBeOfType<InitializerExpressionSyntax>();
+            replacement.Expressions.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void ShouldNotMutateEmptyInitializer()
+        {
+            var emptyInitializerExpression = SyntaxFactory.InitializerExpression(SyntaxKind.ArrayInitializerExpression,
+                SyntaxFactory.SeparatedList<ExpressionSyntax>());
+            var target = new InitializerMutator();
+
+            var result = target.ApplyMutations(emptyInitializerExpression);
+
+            result.ShouldBeEmpty();
+        }
+    }
+}
