dc83115db53edcf47f38d19a065d15b96b457549
Rouke Broersma <mobrockers@gmail.com>
2020-05-01 17:51:00 +0200

    Add support for full framework and build in parallel

--- src/Stryker.Core/Stryker.Core/Initialisation/ProjectOrchestrator.cs
+++ src/Stryker.Core/Stryker.Core/Initialisation/ProjectOrchestrator.cs
@@ -5,9 +5,11 @@ using Stryker.Core.MutationTest;
 using Stryker.Core.Options;
 using Stryker.Core.Reporters;
 using Stryker.Core.Testing;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Stryker.Core.Initialisation
 {
@@ -41,26 +43,71 @@ namespace Stryker.Core.Initialisation
                 _logger.LogInformation("Identifying projects to mutate.");
                 var manager = _buildalyzerProvider.Provide(options.SolutionPath);
 
-                var projects = manager.Projects.Where(x => !x.Value.ProjectFile.PackageReferences.Any(y => y.Name.ToLower().Contains("microsoft.net.test.sdk"))).Select(x => x.Value).ToList();
-                _logger.LogDebug("Found {0} projects", projects.Count);
+                // build all projects
+                var projectsAnalyzerResults = new List<IAnalyzerResult>();
+                _logger.LogDebug("Analysing {count} projects", manager.Projects.Count);
 
-                var testProjectsAnalyzerResults = manager.Projects.Select(x => x.Value).Except(projects).Select(x => x.Build()).ToList();
-                _logger.LogDebug("Found {0} test projects", testProjectsAnalyzerResults.Count);
-
-                foreach (var project in projects)
+                Parallel.ForEach(manager.Projects, project =>
                 {
-                    var relatedTestProjects = testProjectsAnalyzerResults.Where(x => x.Results.FirstOrDefault().ProjectReferences.Any(y => y == project.ProjectFile.Path)).ToList();
+                    _logger.LogDebug("Analysing {projectFilePath}", project.Value.ProjectFile.Path);
+                    var projectAnalyzerResult = project.Value.Build().FirstOrDefault();
+                    if (projectAnalyzerResult is { })
+                    {
+                        projectsAnalyzerResults.Add(projectAnalyzerResult);
+                        _logger.LogDebug("Analysis of project {projectFilePath} succeeded", project.Value.ProjectFile.Path);
+                    }
+                    else
+                    {
+                        _logger.LogWarning("Analysis of project {projectFilePath} failed", project.Value.ProjectFile.Path);
+                    }
+                });
+
+                var projectsUnderTestAnalyzerResults = new List<IAnalyzerResult>();
+                foreach (var project in projectsAnalyzerResults)
+                {
+                    if (project.Properties.ContainsKey("IsTestProject"))
+                    {
+                        if (project.Properties["IsTestProject"].Equals(
+                            "False",
+                            StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            projectsUnderTestAnalyzerResults.Add(project);
+                        }
+                    }
+                    else if (project.Properties.ContainsKey("ProjectTypeGuids"))
+                    {
+                        if (!project.Properties["ProjectTypeGuids"]
+                            .Contains("{3AC096D0-A1C2-E12C-1390-A8335801FDAB}"))
+                        {
+                            projectsUnderTestAnalyzerResults.Add(project);
+                        }
+                    }
+                    else
+                    {
+                        projectsUnderTestAnalyzerResults.Add(project);
+                    }
+                }
+
+                var testProjectsAnalyzerResults = projectsAnalyzerResults.Except(projectsUnderTestAnalyzerResults);
+
+                _logger.LogDebug("Found {0} projects", projectsUnderTestAnalyzerResults.Count());
+                _logger.LogDebug("Found {0} test projects", testProjectsAnalyzerResults.Count());
+
+                foreach (var project in projectsUnderTestAnalyzerResults)
+                {
+                    var projectFilePath = project.ProjectFilePath;
+                    var relatedTestProjects = testProjectsAnalyzerResults.Where(x => x.ProjectReferences.Any(y => y == projectFilePath));
                     if (relatedTestProjects.Any())
                     {
-                        _logger.LogDebug("Matched {0} to {1} test projects:", project.ProjectFile.Path, relatedTestProjects.Count);
+                        _logger.LogDebug("Matched {0} to {1} test projects:", projectFilePath, relatedTestProjects.Count());
                         foreach (var relatedTestProjectAnalyzerResults in relatedTestProjects)
                         {
-                            _logger.LogDebug("{0}", relatedTestProjectAnalyzerResults.Results.FirstOrDefault().ProjectFilePath);
+                            _logger.LogDebug("{0}", relatedTestProjectAnalyzerResults.ProjectFilePath);
                         }
                         var projectOptions = options.ToProjectOptions(
-                            basePath: project.ProjectFile.Path,
-                            projectUnderTest: project.ProjectFile.Path,
-                            testProjects: relatedTestProjects.Select(x => x.Results.FirstOrDefault().ProjectFilePath));
+                            basePath: projectFilePath,
+                            projectUnderTest: projectFilePath,
+                            testProjects: relatedTestProjects.Select(x => x.ProjectFilePath));
                         yield return PrepareProject(projectOptions, reporters);
                     }
                 }
