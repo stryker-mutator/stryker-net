f8779ac761b4ede3b112331c0201e4930330df57
Stijn Rutten <Stijn.Rutten@infosupport.com>
2020-05-04 14:58:44 +0200

    .

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/MutantFilters/MutantFilterFactoryTests.cs
@@ -0,0 +1,64 @@
+﻿using Stryker.Core.MutantFilters;
+using Stryker.Core.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Shouldly;
+using System.Linq;
+using Moq;
+using Stryker.Core.DiffProviders;
+
+namespace Stryker.Core.UnitTest.MutantFilters
+{
+    public class MutantFilterFactoryTests
+    {
+        [Fact]
+        public void MutantFilterFactory_Creates_of_type_BroadcastFilter()
+        {
+            var result = MutantFilterFactory.Create(new StrykerOptions());
+
+            result.ShouldBeOfType<BroadcastMutantFilter>();
+        }
+
+        [Fact]
+        public void Create_Throws_ArgumentNullException_When_Stryker_Options_Is_Null()
+        {
+            var result = Should.Throw<ArgumentNullException>(() => MutantFilterFactory.Create(null));
+        }
+
+        [Fact]
+        public void MutantFilterFactory_Creates_Standard_Mutant_Filters()
+        {
+            // Arrange
+            var strykerOptions = new StrykerOptions(diff: false);
+
+            // Act
+            var result = MutantFilterFactory.Create(strykerOptions);
+
+            // Assert
+            var resultAsBroadcastFilter = result as BroadcastMutantFilter;
+
+            resultAsBroadcastFilter.MutantFilters.Count().ShouldBe(4);
+        }
+
+        [Fact]
+        public void MutantFilterFactory_Creates_DiffMutantFilter_When_Diff_Enabled()
+        {
+            // Arrange
+            var strykerOptions = new StrykerOptions(diff: true);
+
+            var diffProviderMock = new Mock<IDiffProvider>(MockBehavior.Loose);
+
+            // Act
+            var result = MutantFilterFactory.Create(strykerOptions, diffProviderMock.Object);
+
+            // Assert
+            var resultAsBroadcastFilter = result as BroadcastMutantFilter;
+
+            resultAsBroadcastFilter.MutantFilters.Count().ShouldBe(5);
+
+            resultAsBroadcastFilter.MutantFilters.Where(x => x.GetType() == typeof(DiffMutantFilter)).Count().ShouldBe(1);
+        }
+    }
+}
---
+++ src/Stryker.Core/Stryker.Core/MutantFilters/MutantFilterFactory.cs
@@ -0,0 +1,41 @@
+﻿using Stryker.Core.DiffProviders;
+using Stryker.Core.Options;
+using System;
+using System.Collections.Generic;
+
+namespace Stryker.Core.MutantFilters
+{
+    public static class MutantFilterFactory
+    {
+        private static IDiffProvider _diffProvider;
+
+        public static IMutantFilter Create(StrykerOptions options, IDiffProvider diffProvider = null)
+        {
+            if(options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            _diffProvider = diffProvider ?? new GitDiffProvider(options);
+
+            return new BroadcastMutantFilter(DetermineEnabledMutantFilters(options));
+        }
+
+        private static IEnumerable<IMutantFilter> DetermineEnabledMutantFilters(StrykerOptions options)
+        {
+            var enabledFilters = new List<IMutantFilter> {
+                    new FilePatternMutantFilter(),
+                    new IgnoredMethodMutantFilter(),
+                    new ExcludeMutationMutantFilter(),
+                    new ExcludeFromCodeCoverageFilter()
+                };
+
+            if (options.DiffEnabled)
+            {
+                enabledFilters.Add(new DiffMutantFilter(_diffProvider));
+            }
+            
+            return enabledFilters;
+        }
+    }
+}
