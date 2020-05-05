ae755c372066c9517427983ee09295303903278d
Pim Rheiter <pim.rheiter@infosupport.com>
2020-05-05 14:48:13 +0200

    Added RegexMutator

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutators/RegexMutatorTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Shouldly;
+using Stryker.Core.Mutators;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.Mutators
+{
+    public class RegexMutatorTest
+    {
+        [Fact]
+        public void ShouldMutateStringLiteralInRegexConstructor()
+        {
+            var objectCreationExpression = SyntaxFactory.ParseExpression("new Regex(@\"^abc\")") as ObjectCreationExpressionSyntax;
+            var target = new RegexMutator();
+
+            var result = target.ApplyMutations(objectCreationExpression);
+
+            var mutation = result.ShouldHaveSingleItem();
+
+            mutation.DisplayName.ShouldBe("Regex mutation");
+            var replacement = mutation.ReplacementNode.ShouldBeOfType<LiteralExpressionSyntax>();
+            replacement.Token.ValueText.ShouldBe("abc");
+        }
+
+        [Fact]
+        public void ShouldMutateStringLiteralInRegexConstructorWithFullName()
+        {
+            var objectCreationExpression = SyntaxFactory.ParseExpression("new System.Text.RegularExpressions.Regex(@\"^abc\")") as ObjectCreationExpressionSyntax;
+            var target = new RegexMutator();
+
+            var result = target.ApplyMutations(objectCreationExpression);
+
+            var mutation = result.ShouldHaveSingleItem();
+
+            mutation.DisplayName.ShouldBe("Regex mutation");
+            var replacement = mutation.ReplacementNode.ShouldBeOfType<LiteralExpressionSyntax>();
+            replacement.Token.ValueText.ShouldBe("abc");
+        }
+
+
+        [Fact]
+        public void ShouldNotMutateRegexWithoutParameters()
+        {
+            var objectCreationExpression = SyntaxFactory.ParseExpression("new Regex()") as ObjectCreationExpressionSyntax;
+            var target = new RegexMutator();
+            var result = target.ApplyMutations(objectCreationExpression);
+
+            result.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void ShouldNotMutateStringLiteralInOtherConstructor()
+        {
+            var objectCreationExpression = SyntaxFactory.ParseExpression("new Other(@\"^abc\")") as ObjectCreationExpressionSyntax;
+            var target = new RegexMutator();
+            var result = target.ApplyMutations(objectCreationExpression);
+
+            result.ShouldBeEmpty();
+        }
+    }
+}
---
+++ src/Stryker.Core/Stryker.Core/Mutators/RegexMutator.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Stryker.Core.Mutants;
+using Stryker.RegexMutators;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Stryker.Core.Mutators
+{
+    public class RegexMutator : MutatorBase<ObjectCreationExpressionSyntax>, IMutator
+    {
+        public override IEnumerable<Mutation> ApplyMutations(ObjectCreationExpressionSyntax node)
+        {
+            string name = GetTypeName(node);
+            if (name == typeof(Regex).Name || name == typeof(Regex).FullName)
+            {
+                var patternExpression = node.ArgumentList.Arguments.FirstOrDefault()?.Expression;
+                if (patternExpression?.Kind() == SyntaxKind.StringLiteralExpression)
+                {
+                    var currentValue = ((LiteralExpressionSyntax)patternExpression).Token.ValueText;
+                    var regexMutantOrchestrator = new RegexMutantOrchestrator(currentValue);
+                    var replacementValues = regexMutantOrchestrator.Mutate();
+                    foreach (string replacementValue in replacementValues)
+                    {
+                        yield return new Mutation()
+                        {
+                            OriginalNode = patternExpression,
+                            ReplacementNode = patternExpression.ReplaceNode(patternExpression, SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal(replacementValue))),
+                            DisplayName = "Regex mutation",
+                            Type = Mutator.Regex
+                        };
+                    }
+                }
+            }
+        }
+
+        private string GetTypeName(ObjectCreationExpressionSyntax node)
+        {
+            if (node.Type.Kind() == SyntaxKind.IdentifierName)
+            {
+                return ((IdentifierNameSyntax)node.Type).Identifier.ValueText;
+            }
+
+            else if (node.Type.Kind() == SyntaxKind.QualifiedName)
+            {
+                var qualifiedName = (QualifiedNameSyntax)node.Type;
+                var name = qualifiedName.Right.Identifier.ValueText;
+                while (qualifiedName.Left.Kind() == SyntaxKind.QualifiedName)
+                {
+                    qualifiedName = (QualifiedNameSyntax)qualifiedName.Left;
+                    name = $"{qualifiedName.Right.Identifier.ValueText}.{name}";
+                }
+                return $"{((IdentifierNameSyntax)qualifiedName.Left).Identifier.ValueText}.{name}";
+            }
+
+            return null;
+        }
+    }
+}
