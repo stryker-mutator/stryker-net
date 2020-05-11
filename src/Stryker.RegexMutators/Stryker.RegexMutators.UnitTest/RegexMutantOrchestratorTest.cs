5504f60373b96c7d941bae0bbf31dfdd536dbaf8
Pim Rheiter <pim.rheiter@infosupport.com>
2020-05-11 16:21:51 +0200

    Added QuantifierRemoval, CharacterClassNegation and CharacterClassShorthandNegation mutators to orchestrator.

--- src/Stryker.RegexMutators/Stryker.RegexMutators.UnitTest/RegexMutantOrchestratorTest.cs
+++ src/Stryker.RegexMutators/Stryker.RegexMutators.UnitTest/RegexMutantOrchestratorTest.cs
@@ -1,4 +1,5 @@
 ﻿using Shouldly;
+using System.Linq;
 using Xunit;
 
 namespace Stryker.RegexMutators.UnitTest
@@ -6,7 +7,7 @@ namespace Stryker.RegexMutators.UnitTest
     public class RegexMutantOrchestratorTest
     {
         [Fact]
-        public void Test()
+        public void ShouldRemoveAnchor()
         {
             // Arrange
             var target = new RegexMutantOrchestrator("^abc");
@@ -17,6 +18,107 @@ namespace Stryker.RegexMutators.UnitTest
             // Assert
             var mutation = result.ShouldHaveSingleItem();
             mutation.ShouldBe("abc");
+        }
+
+        [Fact]
+        public void ShouldRemoveQuantifier()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator("abcX?");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.ShouldBe("abcX");
+        }
+
+        [Fact]
+        public void ShouldRemoveLazyQuantifier()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator("abcX??");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.ShouldBe("abcX");
+        }
+
+        [Fact]
+        public void ShouldNegateUnnegatedCharacterClass()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator("abc[XY]");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.ShouldBe("abc[^XY]");
+        }
+
+        [Fact]
+        public void ShouldUnnegateNegatedCharacterClass()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator("abc[^XY]");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.ShouldBe("abc[XY]");
+        }
+
+        [Fact]
+        public void ShouldNegateUnnegatedCharacterClassShorthand()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator(@"abc\d");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.ShouldBe(@"abc\D");
+        }
+
+        [Fact]
+        public void ShouldUnnegateNegatedCharacterClassShorthand()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator(@"abc\D");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            var mutation = result.ShouldHaveSingleItem();
+            mutation.ShouldBe(@"abc\d");
+        }
+
+        [Fact]
+        public void ShouldApplyMultipleMutations()
+        {
+            // Arrange
+            var target = new RegexMutantOrchestrator(@"^[abc]\d?");
+
+            // Act
+            var result = target.Mutate();
+
+            // Assert
+            result.Count().ShouldBe(4);
+            result.ShouldContain(@"[abc]\d?");
+            result.ShouldContain(@"^[^abc]\d?");
+            result.ShouldContain(@"^[abc]\D?");
+            result.ShouldContain(@"^[abc]\d");
         }
     }
 }
--- src/Stryker.RegexMutators/Stryker.RegexMutators/RegexMutantOrchestrator.cs
+++ src/Stryker.RegexMutators/Stryker.RegexMutators/RegexMutantOrchestrator.cs
@@ -1,6 +1,9 @@
 ﻿using RegexParser;
 using RegexParser.Nodes;
 using RegexParser.Nodes.AnchorNodes;
+using RegexParser.Nodes.CharacterClass;
+using RegexParser.Nodes.QuantifierNodes;
+using Stryker.RegexMutators.Mutators;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,13 +25,13 @@ namespace Stryker.RegexMutators
             RegexTree tree = parser.Parse();
             _root = tree.Root;
 
-            foreach (string m in _root.GetDescendantNodes().SelectMany(node => FindMutants(node)))
+            foreach (string mutant in _root.GetDescendantNodes().SelectMany(node => FindMutants(node)))
             {
-                yield return m;
+                yield return mutant;
             }
-            foreach (string m in FindMutants(_root))
+            foreach (string mutant in FindMutants(_root))
             {
-                yield return m;
+                yield return mutant;
             }
         }
 
@@ -37,6 +40,30 @@ namespace Stryker.RegexMutators
             if (regexNode is AnchorNode anchorNode)
             {
                 foreach (string mutant in new AnchorRemovalMutator(_root).ApplyMutations(anchorNode))
+                {
+                    yield return mutant;
+                }
+            }
+
+            else if (regexNode is QuantifierNode quantifierNode)
+            {
+                foreach (string mutant in new QuantifierRemovalMutator(_root).ApplyMutations(quantifierNode))
+                {
+                    yield return mutant;
+                }
+            }
+
+            else if (regexNode is CharacterClassNode characterClassNode)
+            {
+                foreach (string mutant in new CharacterClassNegationMutator(_root).ApplyMutations(characterClassNode))
+                {
+                    yield return mutant;
+                }
+            }
+
+            else if (regexNode is CharacterClassShorthandNode characterClassShorthandNode)
+            {
+                foreach (string mutant in new CharacterClassShorthandNegationMutator(_root).ApplyMutations(characterClassShorthandNode))
                 {
                     yield return mutant;
                 }
