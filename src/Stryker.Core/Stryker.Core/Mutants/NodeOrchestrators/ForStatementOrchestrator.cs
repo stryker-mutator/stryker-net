3ddb25136b6eb20d6583651e1cb78f77b7ccc7b3
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2020-08-31 08:45:09 +0200

    fix(Mutation compile errors): Redesign mutation logic (#1172)

---
+++ src/Stryker.Core/Stryker.Core/Mutants/NodeOrchestrators/ForStatementOrchestrator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Stryker.Core.Mutants.NodeOrchestrators
+{
+    internal class ForStatementOrchestrator: NodeSpecificOrchestrator<ForStatementSyntax>
+    {
+        internal override SyntaxNode OrchestrateMutation(ForStatementSyntax forStatement, MutationContext context)
+        {
+            // for needs special treatments for its incrementer
+            var originalFor = forStatement;
+            forStatement = originalFor.TrackNodes(forStatement.Incrementors);
+            foreach (var incrementer in originalFor.Incrementors)
+            {
+                forStatement = forStatement.ReplaceNode(forStatement.GetCurrentNode(incrementer),
+                    context.MutateNodeAndChildren(incrementer, true));
+            }
+
+            // mutate condition, if any
+            if (forStatement.Condition != null)
+            {
+                forStatement = forStatement.ReplaceNode(forStatement.Condition,
+                    context.MutateNodeAndChildren(forStatement.Condition));
+            }
+
+            // mutate the statement/block
+            forStatement = forStatement.ReplaceNode(forStatement.Statement, context.MutateNodeAndChildren(forStatement.Statement));
+            // and now we replace it
+            return forStatement;
+        }
+    }
+}
---
+++ src/Stryker.Core/Stryker.Core/Mutants/NodeOrchestrators/StaticFieldDeclarationOrchestrator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Stryker.Core.Mutants.NodeOrchestrators
+{
+    internal class StaticFieldDeclarationOrchestrator: NodeSpecificOrchestrator<FieldDeclarationSyntax>
+    {
+        protected override bool CanHandle(FieldDeclarationSyntax t)
+        {
+            return t.Modifiers.Any(x => x.Kind() == SyntaxKind.StaticKeyword);
+        }
+
+        internal override SyntaxNode OrchestrateMutation(FieldDeclarationSyntax node, MutationContext context)
+        {
+            return context.EnterStatic().MutateNodeAndChildren(node);
+        }
+    }
+}
