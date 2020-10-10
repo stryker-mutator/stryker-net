78f693d435961ba67350ea75ed13055e8fcb6eee
Rouke Broersma <mobrockers@gmail.com>
2020-10-11 00:28:05 +0200

    Add dummy change

---
+++ src/Stryker.Core/Stryker.Core/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace Stryker.Core
+{
+    public class Class1
+    {
+        public int OnePlusOne()
+        {
+            return 1 + 1;
+        }
+
+        public int OnePlusTwoIfThree()
+        {
+
+            if (3 == 3)
+            {
+                return 1 + 2;
+            }
+        }
+    }
+}
