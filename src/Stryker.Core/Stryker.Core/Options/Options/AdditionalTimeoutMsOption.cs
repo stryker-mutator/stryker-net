8694dd26c2be8bf9a72a4021b3e3ab3cb4c285b9
Rouke Broersma <mobrockers@gmail.com>
2020-10-23 18:08:35 +0200

    Add null check on additional timeout ms option

--- src/Stryker.Core/Stryker.Core/Options/Options/AdditionalTimeoutMsOption.cs
+++ src/Stryker.Core/Stryker.Core/Options/Options/AdditionalTimeoutMsOption.cs
@@ -1,14 +1,20 @@
-﻿namespace Stryker.Core.Options.Options
+﻿using Stryker.Core.Exceptions;
+
+namespace Stryker.Core.Options.Options
 {
-    class AdditionalTimeoutMSOption : BaseStrykerOption<int>
+    class AdditionalTimeoutMsOption : BaseStrykerOption<int>
     {
-        public AdditionalTimeoutMSOption(int additionalTimeoutMS)
+        public AdditionalTimeoutMsOption(int additionalTimeoutMs)
         {
-            Value = additionalTimeoutMS;
+            if (additionalTimeoutMs < 0)
+            {
+                throw new StrykerInputException("Timeout cannot be negative");
+            }
+            Value = additionalTimeoutMs;
         }
 
-        public override StrykerOption Type => StrykerOption.AdditionalTimeoutMS;
-        public override string HelpText => "Stryker calculates a timeout based on the time the testrun takes before the mutations";
+        public override StrykerOption Type => StrykerOption.AdditionalTimeoutMs;
+        public override string HelpText => "Stryker calculates a timeout based on the total unit test runtime before mutation plus a buffer in case a mutation increases the runtime for a unit test. To prevent infinite loops stryker cancels the testrun if it runs longer than the timeout value. If you experience a lot of timeout you might need to increase the timeout value.";
         public override int DefaultValue => 5000;
     }
 }
