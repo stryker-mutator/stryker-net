f54f5580f2e48b067d75939ff9f932f4cfe2c1da
Rouke Broersma <mobrockers@gmail.com>
2020-10-23 16:47:27 +0200

    Add module name option

---
+++ src/Stryker.Core/Stryker.Core/Options/Options/ModuleNameOption.cs
@@ -0,0 +1,11 @@
+﻿namespace Stryker.Core.Options.Options
+{
+    public class ModuleNameOption : BaseStrykerOption<string>
+    {
+        public override StrykerOption Type => StrykerOption.ModuleName;
+
+        public override string HelpText => "Module name used by reporters. Usually a project in your solution would be a module.";
+
+        public override string DefaultValue => null;
+    }
+}
