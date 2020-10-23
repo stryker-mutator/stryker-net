24fd05064db8914e6142cd986a3cab1455e76317
Rouke Broersma <mobrockers@gmail.com>
2020-10-23 17:49:51 +0200

    Add dashboard api key option

---
+++ src/Stryker.Core/Stryker.Core/Options/Options/DashboardApiKeyOption.cs
@@ -0,0 +1,37 @@
+﻿using Stryker.Core.Exceptions;
+using Stryker.Core.Reporters;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stryker.Core.Options.Options
+{
+    public class DashboardApiKeyOption : BaseStrykerOption<string>
+    {
+        public DashboardApiKeyOption(string apiKey, bool dashboardCompareEnabled, IEnumerable<Reporter> reporters)
+        {
+            if (dashboardCompareEnabled)
+            {
+                if (string.IsNullOrEmpty(apiKey))
+                {
+                    throw new StrykerInputException("When the compare to dashboard feature is enabled an api key required.");
+                }
+            }
+
+            if (reporters.Contains(Reporter.Dashboard))
+            {
+                if (string.IsNullOrEmpty(apiKey))
+                {
+                    throw new StrykerInputException("When the dashboard reporter is enabled an api key is required.");
+                }
+            }
+
+            Value = apiKey;
+        }
+
+        public override StrykerOption Type => StrykerOption.DashboardApiKey;
+
+        public override string HelpText => $"The api key for the stryker dashboard. You can get your key here: https://dashboard.stryker-mutator.io/";
+
+        public override string DefaultValue => null;
+    }
+}
