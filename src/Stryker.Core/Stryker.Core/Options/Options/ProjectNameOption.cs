119d2d35d6b24f41959b86c4dd535f9b1fbb3d25
Rouke Broersma <mobrockers@gmail.com>
2020-10-23 16:50:45 +0200

    Add project name option

---
+++ src/Stryker.Core/Stryker.Core/Options/Options/ProjectNameOption.cs
@@ -0,0 +1,11 @@
+﻿namespace Stryker.Core.Options.Options
+{
+    public class ProjectNameOption : BaseStrykerOption<string>
+    {
+        public override StrykerOption Type => StrykerOption.ProjectName;
+
+        public override string HelpText => "The organizational name for your project. Required when dashboard reporter is turned on. Often the name of your solution.";
+
+        public override string DefaultValue => null;
+    }
+}
