ffc4860a105fae836a4e98ba83cf0138d941e128
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2020-10-23 16:35:00 +0200

    feat(mutation orchestration): Improve mutation orchestration and rollback logic (#1236)
    * wip: migrate if mutant placer to dedicated strategy class
    
    * wip: migrate conditionl mutation control to dedicated engine
    
    * WIP: add arrow expression to body code injector
    
    * WIP: implement expression to body conversion
    
    * WIP: refactor MutateOrchestrator
    
    * WIP: refactorting
    
    * WIP: refactor orchestration
    
    * minor
    
    * fix: end orchestration refactoring
    
    * feat: extend for statement mutation scope
    
    * feat: remove duplicate mutants introduced by NEgateContiionMutator
    
    * doc: add documentations
    
    * Update Stryker.Core.UnitTest.csproj
    
    * chore: minor improvement
    
    * Update src/Stryker.Core/Stryker.Core/Helpers/RoslynHelper.cs
    
    Co-authored-by: Rouke Broersma <Mobrockers@users.noreply.github.com>
    
    * fix: address some review comments
    
    * fix: address review comments
    
    * fix: build error due to use of 'operator' keyword
    
    * fix: restore mutant reporting order based on reason and not on status
    
    Co-authored-by: Rouke Broersma <Mobrockers@users.noreply.github.com>

---
+++ src/Stryker.Core/Stryker.Core/Instrumentation/IfInstrumentationEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Stryker.Core.Instrumentation
+{
+    class IfInstrumentationEngine: BaseEngine<IfStatementSyntax>
+    {
+        public IfInstrumentationEngine(string annotation) : base(annotation, "IfInstrumentation")
+        {
+        }
+
+        public IfStatementSyntax InjectIf(ExpressionSyntax condition, StatementSyntax originalNode, StatementSyntax mutatedNode)
+        {
+            return SyntaxFactory.IfStatement(condition, 
+                AsBlock(mutatedNode), 
+                SyntaxFactory.ElseClause(AsBlock(originalNode))).WithAdditionalAnnotations(Marker);
+        }
+
+        private static BlockSyntax AsBlock(StatementSyntax code)
+        {
+            return (code as BlockSyntax) ?? SyntaxFactory.Block(code);
+        }
+
+        protected override SyntaxNode Revert(IfStatementSyntax ifNode)
+        {
+            if (ifNode.Else?.Statement is BlockSyntax block)
+            {
+                return block.Statements.Count == 1 ? block.Statements[0] : block;
+            }
+            throw new InvalidOperationException($"Expected a block containing an 'else' statement, found:\n{ifNode.ToFullString()}.");
+        }
+    }
+}
