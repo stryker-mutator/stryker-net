0e5ea6086b51d3ccee01c646d0f00b06a51630a4
Rouke Broersma <mobrockers@gmail.com>
2020-10-24 00:07:14 +0200

    Add test projects option

---
+++ src/Stryker.Core/Stryker.Core/Options/Options/TestProjectsOption.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Stryker.Core.Options.Options
+{
+    public class TestProjectsOption : BaseStrykerOption<IEnumerable<string>>
+    {
+        public TestProjectsOption(IEnumerable<string> paths)
+        {
+            Value = paths?
+                .Where(path => !path.IsNullOrEmptyInput())
+                .Select(path => FilePathUtils.NormalizePathSeparators(Path.GetFullPath(path)))
+                ?? DefaultValue;
+        }
+
+        public override StrykerOption Type => StrykerOption.TestProjects;
+
+        public override string HelpText => "Specify which test projects should run on the project under test.";
+
+        public override IEnumerable<string> DefaultValue => Enumerable.Empty<string>();
+    }
+}
