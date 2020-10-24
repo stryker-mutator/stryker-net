5333e4614e049ea6f76684afac14cb540c08b858
Pim van Gurp <pim@pimify.nl>
2020-10-24 18:04:35 +0200

    Add regex look ahead and behind mutators
    In regex there is a way to match something, only if it is preceded or proceded by a group.
    For example: (?<=foo)bar will match the word bar in foobar, but won't match bar in minibar.
    
    This mutator flips these regexes to check at the end instead of beginning, and to ensure
    that it does NOT start with the group. It flips the check.
    
    Closes #1278

---
+++ src/Stryker.RegexMutators/Stryker.RegexMutators.UnitTest/Mutators/LookAroundMutatorTest.cs
@@ -0,0 +1,89 @@
+﻿using RegexParser.Nodes;
+using Shouldly;
+using Stryker.RegexMutators.Mutators;
+using System.Collections.Generic;
+using System.Linq;
+using RegexParser.Nodes.GroupNodes;
+using Xunit;
+
+namespace Stryker.RegexMutators.UnitTest.Mutators
+{
+    public class LookAroundMutatorTest
+    {
+        [Fact]
+        public void FlipsPositiveLookAheadToNegativeLookAhead()
+        {
+            // Arrange
+            var foo = new List<RegexNode>
+            {
+                new CharacterNode('f'), 
+                new CharacterNode('o'), 
+                new CharacterNode('o')
+            };
+            var lookaroundGroupNode = new LookaroundGroupNode(true, true, foo);
+            var rootNode = new ConcatenationNode(lookaroundGroupNode);
+            var target = new LookAroundMutator();
+
+            var expectedResults = new List<string>
+            {
+                "(?!foo)",
+                "(?<=foo)"
+            };
+
+            // Act
+            var mutations = target.ApplyMutations(lookaroundGroupNode, rootNode).ToList();
+
+            // Assert
+            var index = 0;
+            var originalQuantifier = "(?=foo)";
+            foreach (var mutation in mutations)
+            {
+                mutation.OriginalNode.ShouldBe(lookaroundGroupNode);
+                mutation.ReplacementNode.ToString().ShouldBe(expectedResults[index]);
+                mutation.ReplacementPattern.ShouldBe(expectedResults[index]);
+                mutation.DisplayName.ShouldBe("Regex greedy quantifier quantity mutation");
+                mutation.Description.ShouldBe($"Quantifier \"{originalQuantifier}\" was replaced with \"{expectedResults[index]}\" at offset 0.");
+                index++;
+            }
+            mutations.Count.ShouldBe(2);
+        }
+        
+        [Fact]
+        public void FlipsNegativeLookAheadToPositiveLookAhead()
+        {
+            // Arrange
+            var foo = new List<RegexNode>
+            {
+                new CharacterNode('f'), 
+                new CharacterNode('o'), 
+                new CharacterNode('o')
+            };
+            var lookaroundGroupNode = new LookaroundGroupNode(true, false, foo);
+            var rootNode = new ConcatenationNode(lookaroundGroupNode);
+            var target = new LookAroundMutator();
+
+            var expectedResults = new List<string>
+            {
+                "(?=foo)",
+                "(?<!foo)"
+            };
+
+            // Act
+            var mutations = target.ApplyMutations(lookaroundGroupNode, rootNode).ToList();
+
+            // Assert
+            var index = 0;
+            var originalQuantifier = "(?!foo)";
+            foreach (var mutation in mutations)
+            {
+                mutation.OriginalNode.ShouldBe(lookaroundGroupNode);
+                mutation.ReplacementNode.ToString().ShouldBe(expectedResults[index]);
+                mutation.ReplacementPattern.ShouldBe(expectedResults[index]);
+                mutation.DisplayName.ShouldBe("Regex greedy quantifier quantity mutation");
+                mutation.Description.ShouldBe($"Quantifier \"{originalQuantifier}\" was replaced with \"{expectedResults[index]}\" at offset 0.");
+                index++;
+            }
+            mutations.Count.ShouldBe(2);
+        }
+    }
+}
---
+++ src/Stryker.RegexMutators/Stryker.RegexMutators/Mutators/LookAroundMutator.cs
@@ -0,0 +1,29 @@
+﻿using RegexParser.Nodes;
+using System.Collections.Generic;
+using RegexParser.Nodes.GroupNodes;
+
+namespace Stryker.RegexMutators.Mutators
+{
+    public class LookAroundMutator : RegexMutatorBase<LookaroundGroupNode>, IRegexMutator
+    {
+        public override IEnumerable<RegexMutation> ApplyMutations(LookaroundGroupNode node, RegexNode root)
+        {
+            yield return FlipLookAround(node, root, node.Lookahead, ! node.Possitive);
+            yield return FlipLookAround(node, root, ! node.Lookahead, node.Possitive);
+        }
+
+        private RegexMutation FlipLookAround(LookaroundGroupNode node, RegexNode root, bool lookAhead, bool positive)
+        {
+            var replacementNode = new LookaroundGroupNode(lookAhead, positive, node.ChildNodes);
+
+            return new RegexMutation
+            {
+                OriginalNode = node,
+                ReplacementNode = replacementNode,
+                DisplayName = "Regex greedy quantifier quantity mutation",
+                Description = $"Quantifier \"{node}\" was replaced with \"{replacementNode}\" at offset {node.GetSpan().Start}.",
+                ReplacementPattern = root.ReplaceNode(node, replacementNode).ToString()
+            };
+        }
+    }
+}
