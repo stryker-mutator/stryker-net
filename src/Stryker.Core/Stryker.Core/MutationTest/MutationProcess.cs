81534629ab236f53da489b318df182f906697e44
dukedagmor <49711949+dukedagmor@users.noreply.github.com>
2020-10-26 16:29:18 +0100

    Update src/Stryker.Core/Stryker.Core/MutationTest/MutationProcess.cs
    Co-authored-by: Rouke Broersma <Mobrockers@users.noreply.github.com>

--- src/Stryker.Core/Stryker.Core/MutationTest/MutationProcess.cs
+++ src/Stryker.Core/Stryker.Core/MutationTest/MutationProcess.cs
@@ -14,7 +14,7 @@ using System.Linq;
 
 namespace Stryker.Core.MutationTest
 {
-    class MutationProcess : IMutationProcess
+    public class MutationProcess : IMutationProcess
     {
 
         private readonly ProjectComponent<SyntaxTree> _projectInfo;
