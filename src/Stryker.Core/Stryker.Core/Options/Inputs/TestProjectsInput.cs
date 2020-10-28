f06fc3c3ec826c539a786164f6b5af16e1c003a4
Rouke Broersma <mobrockers@gmail.com>
2020-10-28 20:53:42 +0100

    Fix TestProjectsInput

--- src/Stryker.Core/Stryker.Core/Options/Inputs/TestProjectsInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/TestProjectsInput.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,17 +6,10 @@ namespace Stryker.Core.Options.Inputs
 {
     public class TestProjectsInput : SimpleStrykerInput<IEnumerable<string>>
     {
-        static TestProjectsInput()
-        {
-            DefaultValue = ;
-        }
-
         public override StrykerInput Type => StrykerInput.TestProjects;
-        public override IEnumerable<string> DefaultInput { get => base.DefaultInput; protected set => base.DefaultInput = value; }
         public override IEnumerable<string> DefaultValue => Enumerable.Empty<string>();
 
         protected override string Description => "Specify the test projects.";
-        protected override string HelpOptions => throw new System.NotImplementedException();
 
         public TestProjectsInput(IEnumerable<string> paths)
         {
