4ef97178ea219960bc09c558e6bcf6c8faf7efb2
Rouke Broersma <mobrockers@gmail.com>
2020-10-29 20:19:45 +0100

    Fix fallback version input

--- src/Stryker.Core/Stryker.Core/Options/Inputs/FallbackVersionInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/FallbackVersionInput.cs
@@ -1,22 +1,21 @@
-﻿using Stryker.Core.Exceptions;
+using Stryker.Core.Exceptions;
 
 namespace Stryker.Core.Options.Inputs
 {
     public class FallbackVersionInput : SimpleStrykerInput<string>
     {
-        static FallbackVersionInput()
-        {
-            Description = $"Project version used as a fallback when no report could be found based on Git information for the Compare feature in reporters. Can be semver, git commit hash, branch name or anything else to indicate what version of your software you're testing. When you don't specify a fallback version, --git-source will be used as fallback version. Example: If the current branch is based on the master branch, set 'master' as the fallback version";
-            DefaultValue = new FallbackVersionInput(DefaultInput, GitDiffTargetInput.DefaultValue).Value;
-        }
-
         public override StrykerInput Type => StrykerInput.FallbackVersion;
+
+        protected override string Description => @"Project version used as a fallback when no report could be found based on Git information for the compare feature.
+Can be semver, git commit hash, branch name or anything else to indicate what version of your software you're testing.
+When you don't specify a fallback version the git diff target will be used as fallback version.
+Example: If the current branch is based on the master branch, set 'master' as the fallback version";
 
         public FallbackVersionInput(string fallbackVersion, string gitDiffTarget)
         {
-            if (fallbackVersion.IsNullOrEmptyInput())
+            if (fallbackVersion is { } && fallbackVersion.IsNullOrEmptyInput())
             {
-                throw new StrykerInputException($"Fallback version cannot be empty. Either fill the option or leave it out.");
+                throw new StrykerInputException($"Fallback version cannot be empty. Either fill the option or leave it out to use the default.");
             }
 
             Value = fallbackVersion ?? gitDiffTarget;
