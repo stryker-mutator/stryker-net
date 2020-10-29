3253005f4d71b33cce02d57702b41b5224b5ecd0
Rouke Broersma <mobrockers@gmail.com>
2020-10-29 21:00:05 +0100

    Fix timeout input

--- src/Stryker.Core/Stryker.Core/Options/Inputs/AdditionalTimeoutMsInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/AdditionalTimeoutMsInput.cs
@@ -1,16 +1,14 @@
-﻿using Stryker.Core.Exceptions;
+using Stryker.Core.Exceptions;
 
 namespace Stryker.Core.Options.Inputs
 {
     public class AdditionalTimeoutMsInput : SimpleStrykerInput<int>
     {
-        static AdditionalTimeoutMsInput()
-        {
-            Description = $"Stryker calculates a timeout based on the time the testrun takes before the mutations | Options {DefaultValue}";
-            DefaultValue = 5000;
-        }
-
         public override StrykerInput Type => StrykerInput.AdditionalTimeoutMs;
+
+        protected override string Description => @"A timeout is calculated based on the initial unit test run before mutating.
+To prevent infinite loops stryker cancels a testrun if it runs longer than the timeout value.
+If you experience a lot of timeout you might need to increase the timeout value.";
 
         public AdditionalTimeoutMsInput(int? additionalTimeoutMs)
         {
