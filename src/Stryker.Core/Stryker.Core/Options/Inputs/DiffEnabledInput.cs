46db9ff7473d2c1d2b55d4ba38edf5f8b52cf06c
Rouke Broersma <mobrockers@gmail.com>
2020-10-29 20:29:30 +0100

    Fix diffenabled input

--- src/Stryker.Core/Stryker.Core/Options/Inputs/DiffEnabledInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/DiffEnabledInput.cs
@@ -1,17 +1,18 @@
-﻿namespace Stryker.Core.Options.Inputs
+namespace Stryker.Core.Options.Inputs
 {
     public class DiffEnabledInput : SimpleStrykerInput<bool>
     {
-        static DiffEnabledInput()
-        {
-            Description = @"Enables the diff feature. It makes sure to only mutate changed files. Gets the diff from git by default.";
-        }
-
         public override StrykerInput Type => StrykerInput.DiffCompare;
+        public override bool DefaultValue => false;
+
+        protected override string Description => "Enables the diff feature. It makes sure to only mutate changed files. Gets the diff from git by default.";
 
         public DiffEnabledInput(bool? diffEnabled)
         {
-            Value = diffEnabled ?? DefaultValue;
+            if (diffEnabled is { })
+            {
+                Value = diffEnabled.Value;
+            }
         }
     }
 }
