fdb37596774e5b9e32f067e58a284c63bc591ebb
Rouke Broersma <mobrockers@gmail.com>
2020-10-29 20:31:44 +0100

    Fix DevMode input

--- src/Stryker.Core/Stryker.Core/Options/Inputs/DevModeInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/DevModeInput.cs
@@ -1,19 +1,19 @@
-﻿namespace Stryker.Core.Options.Inputs
+namespace Stryker.Core.Options.Inputs
 {
     public class DevModeInput : SimpleStrykerInput<bool>
     {
-        static DevModeInput()
-        {
-            Description = @"Stryker automatically removes all mutations from a method if a failed mutation could not be rolled back
-    Setting this flag makes stryker not remove the mutations but rather break on failed rollbacks";
-            DefaultValue = false;
-        }
-
         public override StrykerInput Type => StrykerInput.DevMode;
+        public override bool DefaultValue => false;
+
+        protected override string Description => @"Stryker automatically removes all mutations from a method if a failed mutation could not be rolled back
+    Setting this flag makes stryker not remove the mutations but rather crash on failed rollbacks";
 
         public DevModeInput(bool? devMode)
         {
-            Value = devMode ?? DefaultValue;
+            if (devMode is { })
+            {
+                Value = devMode.Value;
+            }
         }
     }
 }
