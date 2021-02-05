54497919abc1d70983d137714e94138eb9e82737
Richard Werkman <Richard.Werkman@infosupport.com>
2021-02-05 16:02:30 +0100

    Add tests

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/Reporters/StatusReporterTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using Stryker.Core.Mutants;
+using Stryker.Core.Options;
+using Stryker.Core.ProjectComponents;
+using Stryker.Core.Reporters;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.Reporters
+{
+    public class StatusReporterTests
+    {
+        private Mock<ILogger<StatusReporter>> _loggerMock = new Mock<ILogger<StatusReporter>>();
+
+        public StatusReporterTests()
+        {
+        }
+
+        [Fact]
+        public void ShouldPrintNoMutations()
+        {
+            var target = new StatusReporter(new StrykerOptions(), _loggerMock.Object);
+
+
+            var folder = new FolderComposite();
+            folder.Add(new FileLeaf()
+            {
+                Mutants = new Collection<Mutant>()
+                {
+                }
+            });
+
+            target.OnMutantsCreated(folder);
+
+            _loggerMock.Verify(LogLevel.Information, "0     total mutants will be tested", Times.Once);
+            _loggerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldPrintIgnoredStatus()
+        {
+            var target = new StatusReporter(new StrykerOptions(), _loggerMock.Object);
+
+
+            var folder = new FolderComposite();
+            folder.Add(new FileLeaf()
+            {
+                Mutants = new Collection<Mutant>()
+                {
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "In excluded file" },
+                }
+            });
+
+            target.OnMutantsCreated(folder);
+
+            _loggerMock.Verify(LogLevel.Information, "1     mutants got status Ignored.      Reason: In excluded file", Times.Once);
+            _loggerMock.Verify(LogLevel.Information, "1     total mutants are skipped for the above mentioned reasons", Times.Once);
+            _loggerMock.Verify(LogLevel.Information, "0     total mutants will be tested", Times.Once);
+            _loggerMock.VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public void ShouldPrintEachReasonWithCount()
+        {
+            var target = new StatusReporter(new StrykerOptions(), _loggerMock.Object);
+
+
+            var folder = new FolderComposite();
+            folder.Add(new FileLeaf()
+            {
+                Mutants = new Collection<Mutant>()
+                {
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "In excluded file" },
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "In excluded file" },
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "Mutator excluded" },
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "Mutator excluded" },
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "Mutator excluded" },
+                    new Mutant() { ResultStatus = MutantStatus.CompileError, ResultStatusReason = "CompileError" },
+                    new Mutant() { ResultStatus = MutantStatus.Ignored, ResultStatusReason = "In excluded file" },
+                    new Mutant() { ResultStatus = MutantStatus.NotRun },
+                }
+            });
+
+            target.OnMutantsCreated(folder);
+
+            _loggerMock.Verify(LogLevel.Information, "1     mutants got status CompileError. Reason: CompileError", Times.Once);
+            _loggerMock.Verify(LogLevel.Information, "3     mutants got status Ignored.      Reason: In excluded file", Times.Once);
+            _loggerMock.Verify(LogLevel.Information, "3     mutants got status Ignored.      Reason: Mutator excluded", Times.Once);
+            _loggerMock.Verify(LogLevel.Information, "7     total mutants are skipped for the above mentioned reasons", Times.Once);
+            _loggerMock.Verify(LogLevel.Information, "1     total mutants will be tested", Times.Once);
+            _loggerMock.VerifyNoOtherCalls();
+        }
+    }
+}
