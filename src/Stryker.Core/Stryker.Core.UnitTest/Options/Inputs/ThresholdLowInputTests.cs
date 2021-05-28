5985a0edde1fccb468548cc37587f618dae9b774
Rouke Broersma <Rouke.Broersma@infosupport.com>
2021-05-28 14:33:32 +0200

    Add tests for threshold low input

--- src/Stryker.Core/Stryker.Core.UnitTest/Options/Inputs/ThresholdLowInputTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Options/Inputs/ThresholdLowInputTests.cs
@@ -10,14 +10,49 @@ namespace Stryker.Core.UnitTest.Options.Inputs
         [Theory]
         [InlineData(-1, "Threshold low must be between 0 and 100.")]
         [InlineData(101, "Threshold low must be between 0 and 100.")]
-        [InlineData(61, "Threshold low must be less than or equal to threshold high and more than or equal to threshold break. Current high: 60, low: 61, break: 60.")]
-        public void ShouldValidateThresholdLow(int thresholdLow, string message)
+        public void MustBeBetween0and100(int thresholdLow, string message)
         {
             var ex = Assert.Throws<InputException>(() =>
             {
-                var options = new ThresholdLowInput { SuppliedInput = thresholdLow }.Validate(60, 60);
+                var options = new ThresholdLowInput { SuppliedInput = thresholdLow }.Validate(@break: 0, high: 100);
             });
             ex.Message.ShouldBe(message);
+        }
+
+        [Fact]
+        public void MustBeLessthanOrEqualToThresholdHigh()
+        {
+            var ex = Assert.Throws<InputException>(() =>
+            {
+                var options = new ThresholdLowInput { SuppliedInput = 61 }.Validate(@break: 60, high: 60);
+            });
+            ex.Message.ShouldBe("Threshold low must be less than or equal to threshold high. Current low: 61, high: 60.");
+        }
+
+        [Fact]
+        public void MustBeMoreThanThresholdBreak()
+        {
+            var ex = Assert.Throws<InputException>(() =>
+            {
+                var options = new ThresholdLowInput { SuppliedInput = 59 }.Validate(@break: 60, high: 60);
+            });
+            ex.Message.ShouldBe("Threshold low must be more than or equal to threshold break. Current low: 59, break: 60.");
+        }
+
+        [Fact]
+        public void CanBeEqualToThresholdBreak()
+        {
+            var input = 60;
+            var options = new ThresholdLowInput { SuppliedInput = input }.Validate(@break: 60, high: 100);
+            options.ShouldBe(input);
+        }
+
+        [Fact]
+        public void CanBeEqualToThresholdHigh()
+        {
+            var input = 60;
+            var options = new ThresholdLowInput { SuppliedInput = input }.Validate(@break: 0, high: 60);
+            options.ShouldBe(input);
         }
     }
 }
