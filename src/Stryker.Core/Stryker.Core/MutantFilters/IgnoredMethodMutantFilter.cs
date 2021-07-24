e49c1b5475653650ff297e485f32689449ca3d13
Gregory Bell <grbell@microsoft.com>
2021-07-23 17:48:07 -0700

    Add missing comment

--- src/Stryker.Core/Stryker.Core/MutantFilters/IgnoredMethodMutantFilter.cs
+++ src/Stryker.Core/Stryker.Core/MutantFilters/IgnoredMethodMutantFilter.cs
@@ -18,29 +18,30 @@ namespace Stryker.Core.MutantFilters
         public string DisplayName => "method filter";
         private readonly SyntaxTriviaRemover _triviaRemover = new SyntaxTriviaRemover();
 
-        public IEnumerable<Mutant> FilterMutants(IEnumerable<Mutant> mutants, ReadOnlyFileLeaf file, IStrykerOptions options)
-        {
-            if (!options.IgnoredMethods.Any())
-            {
-                return mutants;
-            }
-
-            return mutants.Where(m => !IsPartOfIgnoredMethodCall(m.Mutation.OriginalNode, options));
-        }
+        public IEnumerable<Mutant> FilterMutants(IEnumerable<Mutant> mutants, ReadOnlyFileLeaf file, IStrykerOptions options) =>
+            options.IgnoredMethods.Any() ?
+                    mutants.Where(m => !IsPartOfIgnoredMethodCall(m.Mutation.OriginalNode, options)) :
+                    mutants;
 
         private bool IsPartOfIgnoredMethodCall(SyntaxNode syntaxNode, IStrykerOptions options) =>
             syntaxNode switch
             {
-                // Check if the current node is an invocation
-                // This will also ignore invokable properties like `Func<bool> MyProp { get;}`
+                // Check if the current node is an invocation. This will also ignore invokable properties like `Func<bool> MyProp { get;}`
                 InvocationExpressionSyntax invocation => MatchesAnIgnoredMethod(_triviaRemover.Visit(invocation.Expression).ToString(), options),
+
+                // Check if the current node is an object creation syntax (constructor invocation).
                 ObjectCreationExpressionSyntax creation => MatchesAnIgnoredMethod(_triviaRemover.Visit(creation.Type) + ".ctor", options),
+
+                // Traverse the tree upwards.
                 SyntaxNode node when node.Parent != null => IsPartOfIgnoredMethodCall(syntaxNode.Parent, options),
                 _ => false,
             };
 
         private static bool MatchesAnIgnoredMethod(string expressionString, IStrykerOptions options) => options.IgnoredMethods.Any(r => r.IsMatch(expressionString));
 
+        /// <summary>
+        /// Removes comments, whitespace, and other junk from a syntax tree.
+        /// </summary>
         private sealed class SyntaxTriviaRemover : CSharpSyntaxRewriter
         {
             public override SyntaxTrivia VisitTrivia(SyntaxTrivia trivia) => default;
