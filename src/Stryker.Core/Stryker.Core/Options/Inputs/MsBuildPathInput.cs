c1d82a4188b5ca9591568bc9aeadc0232c691e5a
Rouke Broersma <Rouke.Broersma@infosupport.com>
2021-09-03 15:51:42 +0200

    Add command line input

---
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/MsBuildPathInput.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stryker.Core.Exceptions;
+
+namespace Stryker.Core.Options.Inputs
+{
+    public class MsBuildPathInput : Input<string>
+    {
+        public override string Default => null;
+        protected override string Description => "The path to the msbuild executable to use to build your .NET Framework application. Not used for .net (core).";
+
+        public string Validate(IFileSystem fileSystem)
+        {
+            if (SuppliedInput is not null)
+            {
+                if(!fileSystem.File.Exists(SuppliedInput))
+                {
+                    throw new InputException($"Given msbuild path '{SuppliedInput}' does not exist. Please provide a valid msbuild path or let stryker locate msbuild automatically. ");
+                }
+                return SuppliedInput;
+            }
+            return Default;
+        }
+    }
+}
