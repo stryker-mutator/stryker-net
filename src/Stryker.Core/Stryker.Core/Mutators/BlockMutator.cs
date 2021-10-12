74bb483afec5aeaa88e6e7ae08c3c98e318893c4
Richard Werkman <Richard.Werkman@elekta.com>
2021-10-12 21:10:50 +0200

    spacing

--- src/Stryker.Core/Stryker.Core/Mutators/BlockMutator.cs
+++ src/Stryker.Core/Stryker.Core/Mutators/BlockMutator.cs
@@ -8,6 +8,7 @@ namespace Stryker.Core.Mutators
     class BlockMutator : MutatorBase<BlockSyntax>, IMutator
     {
         private const string MutationName = "Block removal mutation";
+
         public override MutationLevel MutationLevel => MutationLevel.Basic;
 
         public override IEnumerable<Mutation> ApplyMutations(BlockSyntax node)
