61a155a19f8133da851c732ccb78f831ed4107d9
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2021-10-17 18:23:21 -0300

    Fix merge

--- src/Stryker.Core/Stryker.Core.UnitTest/Initialisation/InputFileResolverTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Initialisation/InputFileResolverTests.cs
@@ -148,8 +148,7 @@ namespace Stryker.Core.UnitTest.Initialisation
 
             var nugetHelperMock = new Mock<INugetHelper>(MockBehavior.Strict);
             var projectFileReaderMock = new Mock<IProjectFileReader>(MockBehavior.Strict);
-            var nugetHelperMock = new Mock<INugetHelper>(MockBehavior.Strict);
-            projectFileReaderMock.Setup(x => x.AnalyzeProject(testProjectPath, null))
+            projectFileReaderMock.Setup(x => x.AnalyzeProject(testProjectPath, null, null))
                 .Returns(TestHelper.SetupProjectAnalyzerResult(
                 projectReferences: new List<string> { projectUnderTestPath },
                 targetFramework: "netcoreapp2.1",
