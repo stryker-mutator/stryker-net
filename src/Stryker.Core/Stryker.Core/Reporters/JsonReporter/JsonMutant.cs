a2789da59e213b87264e159510ea69ff3ff3da61
Rouke Broersma <Rouke.Broersma@infosupport.com>
2021-11-06 18:17:28 +0100

    Prepare extra fields on Json report mutant field

--- src/Stryker.Core/Stryker.Core/Reporters/JsonReporter/JsonMutant.cs
+++ src/Stryker.Core/Stryker.Core/Reporters/JsonReporter/JsonMutant.cs
@@ -1,13 +1,22 @@
-﻿
+
 namespace Stryker.Core.Reporters.Json
 {
     public class JsonMutant
     {
         public string Id { get; set; }
         public string MutatorName { get; set; }
+        public string Description { get; set; }
+
         public string Replacement { get; set; }
         public SourceLocation Location { get; set; }
+
         public string Status { get; set; }
-        public string Description { get; set; }
+        public bool IsStatic { get; set; }
+
+        public string[] CoveredBy { get; set; }
+        public string[] KilledBy { get; set; }
+        public int TestsCompleted { get; set; }
+
+        public int Duration { get; set; }
     }
 }
