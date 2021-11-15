46a7f84f9cfc525d23395d3986d2383f4426d98b
Rouke Broersma <mobrockers@gmail.com>
2021-11-15 21:12:30 +0100

    Update src/Stryker.Core/Stryker.Core/MutantFilters/MutantFilter.cs

--- src/Stryker.Core/Stryker.Core/MutantFilters/MutantFilter.cs
+++ src/Stryker.Core/Stryker.Core/MutantFilters/MutantFilter.cs
@@ -1,5 +1,6 @@
 namespace Stryker.Core.MutantFilters
 {
+    // Filters are executed in the order they appear in in this enum. If you change this order you change the order or filter execution
     public enum MutantFilter
     {
         Broadcast,
