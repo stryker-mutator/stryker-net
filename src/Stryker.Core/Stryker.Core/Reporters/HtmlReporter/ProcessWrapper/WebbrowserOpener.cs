d44223d38c2fdf653245a75a444bf421658ea91d
Richard Werkman <Richard.Werkman@infosupport.com>
2021-11-26 17:36:53 +0100

    ExcludeFromCodeCoverage

--- src/Stryker.Core/Stryker.Core/Reporters/HtmlReporter/ProcessWrapper/WebbrowserOpener.cs
+++ src/Stryker.Core/Stryker.Core/Reporters/HtmlReporter/ProcessWrapper/WebbrowserOpener.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Stryker.Core.Reporters.HtmlReporter.ProcessWrapper
 {
+    [ExcludeFromCodeCoverage]
     public class WebbrowserOpener : IWebbrowserOpener
     {
         public Process Open(string path)
