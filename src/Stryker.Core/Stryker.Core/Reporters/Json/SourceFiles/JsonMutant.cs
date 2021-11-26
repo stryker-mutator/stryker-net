5a91a2f38da9a1cfe9668fb2a7c919bcc20c5271
Rouke Broersma <Rouke.Broersma@infosupport.com>
2021-11-26 17:59:35 +0100

    Make aditional Mutant fields available to json report

--- src/Stryker.Core/Stryker.Core/Reporters/Json/SourceFiles/JsonMutant.cs
+++ src/Stryker.Core/Stryker.Core/Reporters/Json/SourceFiles/JsonMutant.cs
@@ -1,4 +1,5 @@
 
+using System.Collections.Generic;
 using Stryker.Core.Mutants;
 
 namespace Stryker.Core.Reporters.Json.SourceFiles
@@ -7,10 +8,20 @@ namespace Stryker.Core.Reporters.Json.SourceFiles
     {
         public string Id { get; init; }
         public string MutatorName { get; init; }
+        public string Description {  get; init; }
+
         public string Replacement { get; init; }
         public Location Location { get; init; }
+
         public string Status { get; init; }
-        public string Description { get; init; }
+        public string StatusReason { get; init; }
+
+        public bool? Static { get; init; }
+
+        public IEnumerable<string> CoveredBy { get; set; }
+        public IEnumerable<string> KilledBy { get; set; }
+        public int? TestsCompleted { get; set; }
+        public int? Duration { get; set; }
 
         public JsonMutant() { }
 
@@ -18,10 +29,15 @@ namespace Stryker.Core.Reporters.Json.SourceFiles
         {
             Id = mutant.Id.ToString();
             MutatorName = mutant.Mutation.DisplayName;
+            Description = mutant.Mutation.Description;
+
             Replacement = mutant.Mutation.ReplacementNode.ToFullString();
             Location = new Location(mutant.Mutation.OriginalNode.GetLocation().GetMappedLineSpan());
+
             Status = mutant.ResultStatus.ToString();
-            Description = mutant.Mutation.Description;
+            StatusReason = mutant.ResultStatusReason;
+
+            Static = mutant.IsStaticValue;
         }
     }
 }
