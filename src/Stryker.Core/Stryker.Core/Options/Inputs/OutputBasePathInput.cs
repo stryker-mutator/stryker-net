40c6ee53ccd1e339879589566d98b4f10a31e644
Yorrick Bakker <y.bakker@me.com>
2022-10-28 15:51:04 +0200

    Remove unused Validate method

--- src/Stryker.Core/Stryker.Core/Options/Inputs/OutputBasePathInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/OutputBasePathInput.cs
@@ -8,17 +8,4 @@ public class OutputBasePathInput : Input<string>
     public override string Default => null;
     protected override string Description => @"Changes the default output path for logs and reports. This can be a relative or absolute path.
 By default, the output is stored in StrykerOutput/ in de working directory.";
-
-    public string Validate(IFileSystem fileSystem)
-    {
-        if (string.IsNullOrWhiteSpace(SuppliedInput))
-        {
-            throw new InputException("Outputpath can't be null or whitespace");
-        }
-        if (!fileSystem.Directory.Exists(SuppliedInput))
-        {
-            throw new InputException("Outputpath should exist");
-        }
-        return SuppliedInput;
-    }
 }
