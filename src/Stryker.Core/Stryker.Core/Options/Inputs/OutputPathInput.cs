0ef8a86f1b71c169f6f23fbbabbc7716256c6985
Yorrick Bakker <y.bakker@me.com>
2022-10-28 16:55:44 +0200

    Remove unnecessary string literal

--- src/Stryker.Core/Stryker.Core/Options/Inputs/OutputPathInput.cs
+++ src/Stryker.Core/Stryker.Core/Options/Inputs/OutputPathInput.cs
@@ -5,7 +5,7 @@ namespace Stryker.Core.Options.Inputs
 {
     public class OutputPathInput : Input<string>
     {
-        protected override string Description => @"Changes the output directory for Stryker reports and logs.";
+        protected override string Description => "Changes the output directory for Stryker reports and logs.";
 
         public override string Default => null;
 
