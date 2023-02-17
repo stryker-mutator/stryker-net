026708a7573b5d8a1d118701fb301788dc4e429f
Liam Rougoor <Liam.Rougoor@infosupport.com>
2023-02-17 11:07:36 +0100

    Change foreach loop to LINQ expression.

--- src/Stryker.Core/Stryker.Core/MutantFilters/IgnoreBlockMutantFilter.cs
+++ src/Stryker.Core/Stryker.Core/MutantFilters/IgnoreBlockMutantFilter.cs
@@ -28,16 +28,12 @@ namespace Stryker.Core.MutantFilters
         public IEnumerable<Mutant> FilterMutants(IEnumerable<Mutant> mutants, IReadOnlyFileLeaf file, StrykerOptions options)
         {
             var blockMutants = mutants.Where(m => m.Mutation.Type == Mutator.Block);
-            var mutantsToIgnore = new List<Mutant>();
-            foreach(var blockMutant in blockMutants)
-            {
-                if (mutants.Any(m => m != blockMutant
+            var mutantsToIgnore = blockMutants.Where(blockMutant => mutants.Any(
+                m => m != blockMutant
                 && !_inactiveStatuses.Contains(m.ResultStatus)
-                && blockMutant.Span.Value.Contains(m.Span.Value)))
-                {
-                    mutantsToIgnore.Add(blockMutant);
-                }
-            }
+                && blockMutant.Span.Value.Contains(m.Span.Value)
+            ));
+
             return mutants.Except(mutantsToIgnore);
         }
     }
