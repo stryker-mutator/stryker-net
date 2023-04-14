3013a94f4a69f1379fe26378585c139919b44b80
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2023-04-14 18:45:33 +0200

    chore: cleanup

--- src/Stryker.Core/Stryker.Core/TestRunners/VsTest/VsTestDescription.cs
+++ src/Stryker.Core/Stryker.Core/TestRunners/VsTest/VsTestDescription.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Stryker.Core.Mutants;
 
