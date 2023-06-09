353bde578a0b98c36e5ee0b9830c9cd3b6408cb5
Rouke Broersma <rouke.broersma@infosupport.com>
2023-06-09 15:33:26 +0200

    fix(report): Use fullPath for file name in json and html report (#2567)
    * fix(report): Use fullPath for file name in json and html report
    
    * Improve error message
    
    * Make paths os specific
    
    * derp

--- src/Stryker.Core/Stryker.Core.UnitTest/Clients/DashboardClientsTest.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Clients/DashboardClientsTest.cs
@@ -13,7 +13,7 @@ using Stryker.Core.ProjectComponents;
 using Stryker.Core.ProjectComponents.TestProjects;
 using Stryker.Core.Reporters;
 using Stryker.Core.Reporters.Json;
-using Stryker.Core.UnitTest.Reporters;
+using Stryker.Core.UnitTest.Reporters.Json;
 using Xunit;
 
 namespace Stryker.Core.UnitTest.Clients
