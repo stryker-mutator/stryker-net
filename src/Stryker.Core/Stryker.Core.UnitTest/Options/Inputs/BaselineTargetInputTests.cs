0e6f2437eabc522e32c084060df45572528a5078
Dani Hengeveld <dani.hengeveld@infosupport.com>
2023-10-13 14:53:47 +0200

    add: tests for baseline target input.
    change: target input default to 'master' to stay consistent with since target input.

---
+++ src/Stryker.Core/Stryker.Core.UnitTest/Options/Inputs/BaselineTargetInputTests.cs
@@ -0,0 +1,52 @@
+﻿using Shouldly;
+using Stryker.Core.Exceptions;
+using Stryker.Core.Options.Inputs;
+using Xunit;
+
+namespace Stryker.Core.UnitTest.Options.Inputs;
+
+public class BaselineTargetInputTests : TestBase
+{
+    [Fact]
+    public void ShouldHaveHelpText()
+    {
+        var target = new BaselineTargetInput();
+        target.HelpText.ShouldBe(
+            "The target branch/commit to compare with the current codebase when the baseline feature is enabled. | default: 'master'");
+    }
+
+    [Fact]
+    public void ShouldUseSuppliedInputWhenSinceEnabled()
+    {
+        var suppliedInput = "develop";
+        var validatedBaselineBranch =
+            new BaselineTargetInput { SuppliedInput = suppliedInput }.Validate(sinceEnabled: true);
+        validatedBaselineBranch.ShouldBe(suppliedInput);
+    }
+
+    [Fact]
+    public void ShouldUseDefaultWhenSinceEnabledAndInputNull()
+    {
+        var input = new BaselineTargetInput();
+        var validatedBaselineBranch = input.Validate(sinceEnabled: true);
+        validatedBaselineBranch.ShouldBe(input.Default);
+    }
+
+    [Fact]
+    public void MustNotBeEmptyStringWhenSinceEnabled()
+    {
+        var ex = Assert.Throws<InputException>(() =>
+        {
+            new BaselineTargetInput { SuppliedInput = "" }.Validate(sinceEnabled: true);
+        });
+        ex.Message.ShouldBe("The baseline target cannot be empty when the since feature is enabled");
+    }
+
+    [Fact]
+    public void ShouldNotValidateBaselineTargetWhenSinceDisabled()
+    {
+        var input = new BaselineTargetInput { SuppliedInput = "develop" };
+        var validatedBaselineBranch = input.Validate(sinceEnabled: false);
+        validatedBaselineBranch.ShouldBe(input.Default);
+    }
+}
