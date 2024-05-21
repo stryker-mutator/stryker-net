a400386859e0668ce7f6dbb96f5b1a2a2b512a9e
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2024-05-21 14:40:01 +0200

    fix: use dotnet msbuild intead of msbuild.Exe on non windows platforms #2938

--- src/Stryker.Core/Stryker.Core/Initialisation/InitialBuildProcess.cs
+++ src/Stryker.Core/Stryker.Core/Initialisation/InitialBuildProcess.cs
@@ -3,6 +3,7 @@ using Stryker.Core.Exceptions;
 using Stryker.Core.Logging;
 using Stryker.Core.Testing;
 using Stryker.Core.ToolHelpers;
+using System;
 using System.IO;
 
 namespace Stryker.Core.Initialisation;
@@ -31,7 +32,6 @@ public class InitialBuildProcess : IInitialBuildProcess
 
         ProcessResult result;
         string buildCommand;
-        string buildPath;
         string options;
         if (fullFramework)
         {
@@ -41,30 +41,34 @@ public class InitialBuildProcess : IInitialBuildProcess
             }
             result = BuildSolutionWithMsBuild(configuration, ref solutionPath, ref msbuildPath, out options);
             buildCommand = msbuildPath;
-            buildPath = solutionPath;
         }
         else
         {
-            buildPath = QuotesIfNeeded(!string.IsNullOrEmpty(solutionPath) ? solutionPath : Path.GetFileName(projectPath));
+            var buildPath = QuotesIfNeeded(!string.IsNullOrEmpty(solutionPath) ? solutionPath : Path.GetFileName(projectPath));
             buildCommand = "dotnet";
 
             _logger.LogDebug("Initial build using path: {buildPath}", buildPath);
-            options = "build ";
             if (!string.IsNullOrEmpty(configuration))
             {
-                options = $"build -c {QuotesIfNeeded(configuration)} ";
+                options = $"build {buildPath} -c {QuotesIfNeeded(configuration)}";
+            }
+            else
+            {
+                options = $"build {buildPath}";
             }
             // Build with dotnet build
-            result = _processExecutor.Start(Path.GetDirectoryName(projectPath), buildCommand, options+buildPath);
+            result = _processExecutor.Start(Path.GetDirectoryName(projectPath), buildCommand, options);
             if (result.ExitCode!=ExitCodes.Success && !string.IsNullOrEmpty(solutionPath))
             {
+                // dump previous build result
+                _logger.LogTrace("Initial build output: {0}", result.Output);
                 _logger.LogWarning("Dotnet build failed, trying with MsBuild.");
                 buildCommand = msbuildPath;
                 result = BuildSolutionWithMsBuild(configuration, ref solutionPath, ref msbuildPath, out options);
             }
 
         }
-        CheckBuildResult(result, buildCommand, options, buildPath);
+        CheckBuildResult(result, buildCommand, options);
     }
 
     private ProcessResult BuildSolutionWithMsBuild(string configuration, ref string solutionPath,
@@ -73,38 +77,46 @@ public class InitialBuildProcess : IInitialBuildProcess
         solutionPath = Path.GetFullPath(solutionPath);
         var solutionDir = Path.GetDirectoryName(solutionPath);
         solutionPath = QuotesIfNeeded(solutionPath);
-        msbuildPath ??= new MsBuildHelper().GetMsBuildPath(_processExecutor);
-        options = string.Empty;
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            msbuildPath ??= new MsBuildHelper().GetMsBuildPath(_processExecutor);
+            options = solutionPath;
+        }
+        else
+        {
+            msbuildPath = "dotnet";
+            options = $"msbuild {solutionPath}";
+        }
         if (!string.IsNullOrEmpty(configuration))
         {
-            options = $" /property:Configuration={QuotesIfNeeded(configuration)} ";
+            options += $" /property:Configuration={QuotesIfNeeded(configuration)}";
         }
         // Build project with MSBuild.exe
-        var result = _processExecutor.Start(solutionDir, msbuildPath, $"{solutionPath}{options}");
+        var result = _processExecutor.Start(solutionDir, msbuildPath, options);
         if (result.ExitCode != ExitCodes.Success)
         {
             _logger.LogWarning("MsBuild failed to build the solution, trying to restore packages and build again.");
-            _processExecutor.Start(solutionDir, msbuildPath, $"{solutionPath}${options}-t:restore -p:RestorePackagesConfig=true");
+            _processExecutor.Start(solutionDir, msbuildPath, $"${options} -t:restore -p:RestorePackagesConfig=true");
             result = _processExecutor.Start(solutionDir, msbuildPath, solutionPath);
         }
         return result;
     }
 
-    private void CheckBuildResult(ProcessResult result, string buildCommand, string options, string buildPath)
+    private void CheckBuildResult(ProcessResult result, string buildCommand, string options)
     {
         if (result.ExitCode != ExitCodes.Success)
         {
             _logger.LogError("Initial build failed: {0}", result.Output);
             // Initial build failed
-            throw new InputException(result.Output, FormatBuildResultErrorString(buildCommand, options, buildPath));
+            throw new InputException(result.Output, FormatBuildResultErrorString(buildCommand, options));
         }
         _logger.LogTrace("Initial build output {0}", result.Output);
         _logger.LogDebug("Initial build successful");
     }
 
-    private static string FormatBuildResultErrorString(string buildCommand, string options, string buildPath) =>
+    private static string FormatBuildResultErrorString(string buildCommand, string options) =>
         "Initial build of targeted project failed. Please make sure the targeted project is buildable." +
-        $" You can reproduce this error yourself using: \"{QuotesIfNeeded(buildCommand)} {options}{buildPath}\"";
+        $" You can reproduce this error yourself using: \"{QuotesIfNeeded(buildCommand)} {options}\"";
 
     private static string QuotesIfNeeded(string parameter)
     {
