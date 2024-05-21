9d3d59b537d0dc9bf417062443ce42db53f817d4
Marc Zwerver <Marc.Zwerver@infosupport.com>
2024-05-21 05:55:37 +0200

    Fix(Unittests): Fix unittests dependend on file-scoped namespaces

--- src/Stryker.Core/Stryker.Core/InjectedHelpers/Coverage/MutantContext.cs
+++ src/Stryker.Core/Stryker.Core/InjectedHelpers/Coverage/MutantContext.cs
@@ -2,32 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Stryker;
-
-internal sealed class MutantContext: IDisposable
+namespace Stryker
 {
-    [ThreadStatic] private static int depth;
 
-    public MutantContext()
+    internal sealed class MutantContext : IDisposable
     {
-        depth++;
-    }
+        [ThreadStatic] private static int depth;
 
-    public void Dispose()
-    {
-        depth--;
-    }
-
-    public static bool InStatic()
-    {
-        return depth > 0;
-    }
-
-    public static T TrackValue<T>(Func<T> builder)
-    {
-        using (MutantContext context = new MutantContext())
+        public MutantContext()
         {
-            return builder();
+            depth++;
+        }
+
+        public void Dispose()
+        {
+            depth--;
+        }
+
+        public static bool InStatic()
+        {
+            return depth > 0;
+        }
+
+        public static T TrackValue<T>(Func<T> builder)
+        {
+            using (MutantContext context = new MutantContext())
+            {
+                return builder();
+            }
         }
     }
 }
--- src/Stryker.Core/Stryker.Core/InjectedHelpers/MutantControl.cs
+++ src/Stryker.Core/Stryker.Core/InjectedHelpers/MutantControl.cs
@@ -1,77 +1,79 @@
-namespace Stryker;
-
-public static class MutantControl
+namespace Stryker
 {
-    private static System.Collections.Generic.List<int> _coveredMutants = new System.Collections.Generic.List<int>();
-    private static System.Collections.Generic.List<int> _coveredStaticdMutants = new System.Collections.Generic.List<int>();
-    private static string envName = string.Empty;
-    private static System.Object _coverageLock = new System.Object();
 
-    // this attribute will be set by the Stryker Data Collector before each test
-    public static bool CaptureCoverage;
-    public static int ActiveMutant = -2;
-    public const int ActiveMutantNotInitValue = -2;
-
-    public static void InitCoverage()
+    public static class MutantControl
     {
-        ResetCoverage();
-    }
+        private static System.Collections.Generic.List<int> _coveredMutants = new System.Collections.Generic.List<int>();
+        private static System.Collections.Generic.List<int> _coveredStaticdMutants = new System.Collections.Generic.List<int>();
+        private static string envName = string.Empty;
+        private static System.Object _coverageLock = new System.Object();
 
-    public static void ResetCoverage()
-    {
-        _coveredMutants = new System.Collections.Generic.List<int>();
-        _coveredStaticdMutants = new System.Collections.Generic.List<int>();
-    }
+        // this attribute will be set by the Stryker Data Collector before each test
+        public static bool CaptureCoverage;
+        public static int ActiveMutant = -2;
+        public const int ActiveMutantNotInitValue = -2;
 
-    public static System.Collections.Generic.IList<int>[] GetCoverageData()
-    {
-        System.Collections.Generic.IList<int>[] result = new System.Collections.Generic.IList<int>[]{_coveredMutants, _coveredStaticdMutants};
-        ResetCoverage();
-        return result;
-    }
-
-    private static void CurrentDomain_ProcessExit(object sender, System.EventArgs e)
-    {
-        System.GC.KeepAlive(_coveredMutants);
-        System.GC.KeepAlive(_coveredStaticdMutants);
-    }
-
-    // check with: Stryker.MutantControl.IsActive(ID)
-    public static bool IsActive(int id)
-    {
-        if (CaptureCoverage)
+        public static void InitCoverage()
         {
-            RegisterCoverage(id);
-            return false;
+            ResetCoverage();
         }
-        if (ActiveMutant == ActiveMutantNotInitValue)
-        {
-            #pragma warning disable CS8600
-            string environmentVariable = System.Environment.GetEnvironmentVariable("ActiveMutation");
-            if (string.IsNullOrEmpty(environmentVariable))
-            {
-                ActiveMutant = -1;
-            }
-            else
-            {
-                ActiveMutant = int.Parse(environmentVariable);
-            }
-        }
-        
-        return id == ActiveMutant;
-    }
 
-    private static void RegisterCoverage(int id)
-    {
-        lock (_coverageLock)
+        public static void ResetCoverage()
         {
-            if (!_coveredMutants.Contains(id))
+            _coveredMutants = new System.Collections.Generic.List<int>();
+            _coveredStaticdMutants = new System.Collections.Generic.List<int>();
+        }
+
+        public static System.Collections.Generic.IList<int>[] GetCoverageData()
+        {
+            System.Collections.Generic.IList<int>[] result = new System.Collections.Generic.IList<int>[] { _coveredMutants, _coveredStaticdMutants };
+            ResetCoverage();
+            return result;
+        }
+
+        private static void CurrentDomain_ProcessExit(object sender, System.EventArgs e)
+        {
+            System.GC.KeepAlive(_coveredMutants);
+            System.GC.KeepAlive(_coveredStaticdMutants);
+        }
+
+        // check with: Stryker.MutantControl.IsActive(ID)
+        public static bool IsActive(int id)
+        {
+            if (CaptureCoverage)
             {
-                _coveredMutants.Add(id);
+                RegisterCoverage(id);
+                return false;
             }
-            if (MutantContext.InStatic() && !_coveredStaticdMutants.Contains(id))
+            if (ActiveMutant == ActiveMutantNotInitValue)
             {
-                _coveredStaticdMutants.Add(id);
+#pragma warning disable CS8600
+                string environmentVariable = System.Environment.GetEnvironmentVariable("ActiveMutation");
+                if (string.IsNullOrEmpty(environmentVariable))
+                {
+                    ActiveMutant = -1;
+                }
+                else
+                {
+                    ActiveMutant = int.Parse(environmentVariable);
+                }
+            }
+
+            return id == ActiveMutant;
+        }
+
+        private static void RegisterCoverage(int id)
+        {
+            lock (_coverageLock)
+            {
+                if (!_coveredMutants.Contains(id))
+                {
+                    _coveredMutants.Add(id);
+                }
+                if (MutantContext.InStatic() && !_coveredStaticdMutants.Contains(id))
+                {
+                    _coveredStaticdMutants.Add(id);
+                }
             }
         }
     }
