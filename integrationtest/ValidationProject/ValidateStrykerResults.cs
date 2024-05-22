b09a3a82f504488274389ee2adcedcba3a6c251b
iamdmitrij <3024338+iamdmitrij@users.noreply.github.com>
2024-05-22 08:05:24 +0300

    Fix test results

--- integrationtest/ValidationProject/ValidateStrykerResults.cs
+++ integrationtest/ValidationProject/ValidateStrykerResults.cs
@@ -83,7 +83,7 @@ namespace IntegrationTests
             var report = JsonConvert.DeserializeObject<JsonReport>(strykerRunOutput);
 
             CheckReportMutants(report, total: 130, ignored: 60, survived: 5, killed: 10, timeout: 2, nocoverage: 45);
-            CheckReportTestCounts(report, total: 14);
+            CheckReportTestCounts(report, total: 19);
         }
 
         [Fact]
