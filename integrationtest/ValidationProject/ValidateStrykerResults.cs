b8ff71726e0471b1eb5d5da39f523405415939c1
Richard Werkman <richard.werkman@infosupport.com>
2024-06-07 16:33:50 +0200

    Fix

--- integrationtest/ValidationProject/ValidateStrykerResults.cs
+++ integrationtest/ValidationProject/ValidateStrykerResults.cs
@@ -122,7 +122,7 @@ namespace IntegrationTests
 
             var report = JsonConvert.DeserializeObject<JsonReport>(strykerRunOutput);
 
-            CheckReportMutants(report, total: 567, ignored: 101, survived: 2, killed: 7, timeout: 2, nocoverage: 427);
+            CheckReportMutants(report, total: 567, ignored: 101, survived: 2, killed: 7, timeout: 2, nocoverage: 436);
             CheckReportTestCounts(report, total: 20);
         }
 
@@ -141,7 +141,7 @@ namespace IntegrationTests
 
             var report = JsonConvert.DeserializeObject<JsonReport>(strykerRunOutput);
 
-            CheckReportMutants(report, total: 567, ignored: 239, survived: 2, killed: 5, timeout: 2, nocoverage: 291);
+            CheckReportMutants(report, total: 577, ignored: 241, survived: 2, killed: 5, timeout: 2, nocoverage: 298);
             CheckReportTestCounts(report, total: 22);
         }
 
