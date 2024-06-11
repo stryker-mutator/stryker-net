a9fe1a7ac9d0b56d410bd649604e86930a63e832
Dmitrij <3024338+iamdmitrij@users.noreply.github.com>
2024-06-11 15:51:52 +0300

    Update src/Stryker.Core/Stryker.Core.UnitTest/Mutators/StringMutatorTests.cs
    Co-authored-by: Richard Werkman <Richard1158@gmail.com>

--- src/Stryker.Core/Stryker.Core.UnitTest/Mutators/StringMutatorTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutators/StringMutatorTests.cs
@@ -21,7 +21,7 @@ namespace Stryker.Core.UnitTest.Mutators
         [InlineData(@"""foo""u8", @"""""u8")]
         public void ShouldMutateUtf8StringLiteral(string original, string expected)
         {
-            var syntaxTree = CSharpSyntaxTree.ParseText($"""var test = "{original}"u8;""");
+            var syntaxTree = CSharpSyntaxTree.ParseText($"var test = {original};");
       
             var literalExpression = syntaxTree.GetRoot().DescendantNodes().OfType<LiteralExpressionSyntax>().First();
             var mutator = new StringMutator();
