c453da713e16d8915e006401ead05823e0eb07a4
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2024-08-09 23:22:41 +0200

    chore: add integration tests

---
+++ integrationtest/TargetProjects/NetCore/NetCoreTestProject.XUnit/VariousTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NetCoreTestProject.XUnit;
+public class VariousTests
+{
+    [Fact]
+    public void UseAssertTest()
+    {
+        var target = new TargetProject.StrykerFeatures.UseAssert();
+        target.IncrementCounter();
+        // no assert needed, Debug.Assert will throw if counter is less than 0
+    }
+}
