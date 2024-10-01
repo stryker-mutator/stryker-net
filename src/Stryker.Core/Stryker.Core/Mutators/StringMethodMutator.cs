7eaa0a343675d1528a50c49e3fb87c7d5c290315
unknown <cj.adolfsson@gmail.com>
2024-10-01 21:26:53 +0200

    Added string mutators.

--- src/Stryker.Core/Stryker.Core/Mutators/StringMethodMutator.cs
+++ src/Stryker.Core/Stryker.Core/Mutators/StringMethodMutator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -62,6 +62,8 @@ public class StringMethodMutator : MutatorBase<ExpressionSyntax>
             "ToLowerInvariant" => "ToUpperInvariant",
             "PadLeft" => "PadRight",
             "PadRight" => "PadLeft",
+            "IndexOf" => "LastIndexOf",
+            "LastIndexOf" => "IndexOf",
             _ => null
         };
 
