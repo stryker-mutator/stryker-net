64ca51bd048245697bd4c3870637d3ac6560ca08
Pentiva <git@pentiva.uk>
2024-11-14 11:05:48 +0000

    fix: Remove too complex test case

--- src/Stryker.Core/Stryker.Core.UnitTest/Mutants/GeneratedRegexOrchestratorTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/Mutants/GeneratedRegexOrchestratorTests.cs
@@ -200,15 +200,6 @@ public partial class GeneratedRegexOrchestratorTests : TestBase
                                                    private static partial Regex AbcGeneratedRegex();
                                                }
                                                """],
-        ["ComplexEmailRegexInDedicatedClass",
-            """"
-            using System.Text.RegularExpressions;
-            namespace StrykerNet.UnitTest.Mutants.TestResources;
-            public partial class R {
-                [GeneratedRegex("""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""", RegexOptions.IgnoreCase)]
-                private static partial Regex EmailGeneratedRegex();
-            }
-            """"],
         ["SimpleSingleRegexInNestedClass", """
                                            using System.Text.RegularExpressions;
                                            namespace StrykerNet.UnitTest.Mutants.TestResources;
