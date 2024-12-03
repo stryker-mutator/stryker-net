7461da86ac4017fe9a1a9103de8b9fabd841bc0d
Rouke Broersma <rouke.broersma@infosupport.com>
2024-12-03 09:09:47 +0100

    chore: Delete fsharp support (#3121)
    * Delete fsharp support
    
    * Remove unnecesary changes
    
    * format
    
    * Don't remove neccesary
    
    * Remove fsharp integration test
    
    ---------
    
    Co-authored-by: Richard Werkman <Richard1158@gmail.com>

--- src/Stryker.Core/Stryker.Core/Mutants/FsharpOrchestrators/DefaultOrchestrator.cs
+++
@@ -1,9 +0,0 @@
-namespace Stryker.Core.Mutants.FsharpOrchestrators;
-
-public class DefaultOrchestrator<T> : IFsharpTypeHandler<T>
-{
-    public virtual T Mutate(T input, FsharpMutantOrchestrator iterator)
-    {
-        return input;
-    }
-}
