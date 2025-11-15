22bb187d18618c6bacc75e261a2e8b934f9e2453
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2025-11-15 09:00:18 +0100

    chore: improve log message

--- src/Stryker.Core/Stryker.Core/Mutants/CsharpNodeOrchestrators/CommentParser.cs
+++ src/Stryker.Core/Stryker.Core/Mutants/CsharpNodeOrchestrators/CommentParser.cs
@@ -56,7 +56,7 @@ internal static class CommentParser
                 else
                 {
                     Logger.LogError(
-                        "{Label} not recognized as a mutator at {Location}, {FilePath}. Legal values are {LegalValues}.",
+                        "{Label} not recognized as a mutator at {0}, {1}. Legal values are {2}.",
                         labels[i],
                         node.GetLocation().GetMappedLineSpan().StartLinePosition,
                         node.SyntaxTree.FilePath,
@@ -99,12 +99,12 @@ internal static class CommentParser
         {
             return InterpretStrykerComment(node, context, commentTrivia);
         }
-        catch (TimeoutException)
+        catch (TimeoutException exception)
         {
             Logger.LogWarning(
-                "Parsing Stryker comments at {0}, {1} took too long to parse and was ignored.",
+                "Parsing Stryker comments at {0}, {1} took too long to parse and was ignored ({2}). Comment: {3}",
                 node.GetLocation().GetMappedLineSpan().StartLinePosition,
-                node.SyntaxTree.FilePath);
+                node.SyntaxTree.FilePath, exception.Message, commentTrivia);
             return null;
         }
     }
