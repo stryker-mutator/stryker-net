3d02890776073241b2b95e5577650ca8d604c355
Richard Werkman <Richard.Werkman@Richards-MacBook-Pro.local>
2025-11-28 18:20:16 +0100

    fix compile errors

--- integrationtest/TargetProjects/NetCore/TargetProject/Constructs/CSharp14.cs
+++ integrationtest/TargetProjects/NetCore/TargetProject/Constructs/CSharp14.cs
@@ -2,35 +2,24 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace TargetProject.Constructs;
 
 // https://learn.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-14
-public static class Example
+public static class Enumerable
 {
     // Extension block
-    extension<TSource>(IEnumerable<TSource> source) // extension members for IEnumerable<TSource>
+    extension<T>(IEnumerable<T> source)
+        where T : IEquatable<T>
     {
-        // Extension property:
-        public bool IsEmpty => !source.Any();
-
-        // Extension method:
-        public IEnumerable<TSource> Where(Func<TSource, bool> predicate) { ... }
+        public IEnumerable<T> ValuesEqualTo(T threshold)
+            => source.Where(x => x.Equals(threshold));
     }
+}
 
-    // extension block, with a receiver type only
-    extension<TSource>(IEnumerable<TSource>) // static extension members for IEnumerable<Source>
-    {
-        // static extension method:
-        public static IEnumerable<TSource> Combine(IEnumerable<TSource> first, IEnumerable<TSource> second) { ... }
-
-        // static extension property:
-        public static IEnumerable<TSource> Identity => Enumerable.Empty<TSource>();
-
-        // static user defined operator:
-        public static IEnumerable<TSource> operator + (IEnumerable<TSource> left, IEnumerable<TSource> right) => left.Concat(right);
-    }
-
+public class Example2
+{
     // Field keyword
     public string Message
     {
@@ -45,7 +34,7 @@ public static class Example
     }
 
     // Null conditional assignment
-    public void EnsureListInitialized(Example example)
+    public void EnsureListInitialized(Example2 example)
     {
         example?.Message = "Hello, World!";
     }
