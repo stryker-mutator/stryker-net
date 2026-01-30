6b107c0d95a023fee7adfe50f7a3f900f959f971
Rouke Broersma <Rouke.Broersma@infosupport.com>
2026-01-30 11:55:12 +0100

    code smells

--- src/Stryker.DataCollector/Helpers.cs
+++ src/Stryker.DataCollector/Helpers.cs
@@ -1,13 +1,9 @@
 using System;
-using System.Linq;
 
 namespace Stryker.DataCollector
 {
     internal static class Helpers
     {
-        public static T ExtractAttribute<T>(this Type type) where T : Attribute
-        {
-            return type.GetCustomAttributes(typeof(T), false).First() as T;
-        }
+        public static T ExtractAttribute<T>(this Type type) where T : Attribute => type.GetCustomAttributes(typeof(T), false)[0] as T;
     }
 }
