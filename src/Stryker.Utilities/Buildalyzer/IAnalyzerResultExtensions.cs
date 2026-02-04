ce1e7796603de60eb88138f4d5833da8ad2706ae
Cédric Luthi <cedric.luthi@gmail.com>
2026-02-04 09:57:59 +0100

    Test for IsTestingPlatformApplication first
    As suggested by Youssef Victor (who works on MTP): https://github.com/stryker-mutator/stryker-net/pull/3423#issuecomment-3845798925
    
    > Check IsTestingPlatformApplication first. If true, that is MTP (regardless of the value of IsTestProject)
    >
    > If it's false **and** IsTestProject is true, that is VSTest project.
    >
    > If both are false, it's not a test project at all.

--- src/Stryker.Utilities/Buildalyzer/IAnalyzerResultExtensions.cs
+++ src/Stryker.Utilities/Buildalyzer/IAnalyzerResultExtensions.cs
@@ -172,9 +172,9 @@ public static class IAnalyzerResultExtensions
 
     private static bool IsTestProject(this IAnalyzerResult analyzerResult)
     {
-        if (!bool.TryParse(analyzerResult.GetPropertyOrDefault("IsTestProject"), out var isTestProject))
+        if (!bool.TryParse(analyzerResult.GetPropertyOrDefault("IsTestingPlatformApplication"), out var isTestProject))
         {
-            if (!bool.TryParse(analyzerResult.GetPropertyOrDefault("IsTestingPlatformApplication"), out isTestProject))
+            if (!bool.TryParse(analyzerResult.GetPropertyOrDefault("IsTestProject"), out isTestProject))
             {
                 isTestProject = Array.Exists(KnownTestPackages, n => analyzerResult.PackageReferences.ContainsKey(n));
             }
