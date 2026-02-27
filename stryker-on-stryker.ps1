026856e3af36f3f6b86c19c693a70479bf9f3bbc
Richard Werkman <richard1158@gmail.com>
2026-02-27 11:39:50 +0100

    Set verbosity to debug in stryker-on-stryker pipeline

--- stryker-on-stryker.ps1
+++ stryker-on-stryker.ps1
@@ -69,7 +69,7 @@ function Invoke-Stryker {
     $ToolParameters |
       Where-Object { $_ -is [string] -and $_.Trim().Length -gt 0 }
   )
-  $effectiveToolParameters += '-L'
+  $effectiveToolParameters += '--verbosity debug --log-to-file'
 
   Push-Location $RunDirectory
   Write-Info ("Current working directory: " + (Get-Location))
