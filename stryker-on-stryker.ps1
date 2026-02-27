b42d6d9e7e84cdcb1d18315315ccd87212a1b3eb
Richard Werkman <richard1158@gmail.com>
2026-02-27 09:49:59 +0100

    Switch to file logging in stryker-on-stryker pipeline

--- stryker-on-stryker.ps1
+++ stryker-on-stryker.ps1
@@ -69,7 +69,7 @@ function Invoke-Stryker {
     $ToolParameters |
       Where-Object { $_ -is [string] -and $_.Trim().Length -gt 0 }
   )
-  $effectiveToolParameters += '-V trace'
+  $effectiveToolParameters += '-L'
 
   Push-Location $RunDirectory
   Write-Info ("Current working directory: " + (Get-Location))
