b275d8c9a0b3cc20bf36d6cd7a5bd6913038c5cf
Rouke Broersma <rouke.broersma@infosupport.com>
2026-08-14 20:23:00 +0000

    More tests

--- src/Stryker.Core/Stryker.Core.UnitTest/MutantFilters/BaselineMutantFilterTests.cs
+++ src/Stryker.Core/Stryker.Core.UnitTest/MutantFilters/BaselineMutantFilterTests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Shouldly;
@@ -9,6 +10,7 @@ using Stryker.Abstractions.Baseline;
 using Stryker.Abstractions.Options;
 using Stryker.Abstractions.ProjectComponents;
 using Stryker.Abstractions.Reporting;
+using Stryker.Abstractions.Testing;
 using Stryker.Configuration.Options;
 using Stryker.Core.Baseline.Providers;
 using Stryker.Core.Baseline.Utils;
@@ -19,6 +21,7 @@ using Stryker.Core.ProjectComponents.Csharp;
 using Stryker.Core.ProjectComponents.TestProjects;
 using Stryker.Core.Reporters.Json;
 using Stryker.Core.Reporters.Json.SourceFiles;
+using Stryker.Core.Reporters.Json.TestFiles;
 using Stryker.Core.UnitTest.Reporters.Json;
 
 namespace Stryker.Core.UnitTest.MutantFilters;
@@ -465,5 +468,149 @@ public class BaselineMutantFilterTests : TestBase
 
         // Assert
         results.ShouldHaveSingleItem();
+    }
+
+    [TestMethod]
+    public void FilterMutants_ReusesStatus_WhenCoveringTestIsUnchanged_AndCachesTestFileDiff()
+    {
+        var baselineProvider = new Mock<IBaselineProvider>();
+        var diffProvider = new Mock<IDiffProvider>();
+        var contentMatcher = new Mock<IContentMutantMatcher>();
+        var contentTestMatcher = new Mock<IContentTestMatcher>();
+        var options = new StrykerOptions
+        {
+            WithBaseline = true,
+            ProjectVersion = "version"
+        };
+        var source = "class Tests { void Test() { Assert.IsTrue(true); } }";
+        var baselineMutants = new HashSet<IJsonMutant>
+        {
+            new JsonMutant { Status = "Killed", CoveredBy = ["test-id"] },
+            new JsonMutant { Status = "Killed", CoveredBy = ["test-id"] }
+        };
+        var baselineTestFile = new JsonTestFile
+        {
+            Source = source,
+            Tests = new HashSet<IJsonTest> { new JsonTest("test-id") }
+        };
+        var baseline = new MockJsonReport(null, new Dictionary<string, ISourceFile>
+        {
+            ["foo.cs"] = new MockJsonReportFileComponent("", source, baselineMutants)
+        })
+        {
+            TestFiles = new Dictionary<string, IJsonTestFile> { ["tests.cs"] = baselineTestFile }
+        };
+        var currentTest = CreateCurrentTest(source, "test-id");
+        var currentTestFile = new Mock<ITestFile>();
+        currentTestFile.SetupGet(x => x.RelativePath).Returns("tests.cs");
+        currentTestFile.SetupGet(x => x.Source).Returns(source);
+        currentTestFile.SetupGet(x => x.Tests).Returns(new List<ITestCase> { currentTest });
+        var testProjectsInfo = new Mock<ITestProjectsInfo>();
+        testProjectsInfo.SetupGet(x => x.TestFiles).Returns(new[] { currentTestFile.Object });
+        var gitInfoProvider = new Mock<IGitInfoProvider>();
+        var baselineFile = new CsharpFileLeaf { RelativePath = "foo.cs", SourceCode = source };
+        var mutants = baselineMutants.Select(_ => (IMutant)new Mutant { ResultStatus = MutantStatus.Pending }).ToList();
+
+        baselineProvider.Setup(x => x.Load("baseline/version")).ReturnsAsync(baseline);
+        diffProvider.Setup(x => x.GetContentDiff(source, source)).Returns(EmptyContentDiff);
+        contentMatcher.Setup(x => x.MatchByLocation(It.IsAny<IEnumerable<IMutant>>(), It.IsAny<IJsonMutant>(), EmptyContentDiff)).Returns(mutants);
+        contentTestMatcher.Setup(x => x.IsTestUnchanged(It.IsAny<IJsonTest>(), currentTest, EmptyContentDiff)).Returns(true);
+
+        var target = new BaselineMutantFilter(options, baselineProvider.Object, gitInfoProvider.Object,
+            diffProvider.Object, contentMatcher.Object, testProjectsInfo.Object, contentTestMatcher.Object);
+
+        target.FilterMutants(mutants, baselineFile, options).ToList();
+
+        mutants.ShouldAllBe(mutant => mutant.ResultStatus == MutantStatus.Killed);
+        diffProvider.Verify(x => x.GetContentDiff(source, source), Times.Once);
+        contentTestMatcher.Verify(x => x.IsTestUnchanged(It.IsAny<IJsonTest>(), currentTest, EmptyContentDiff), Times.Exactly(2));
+    }
+
+    [TestMethod]
+    public void FilterMutants_ResetsMutantsToPending_WhenCoveringTestIsChanged()
+    {
+        var baselineProvider = new Mock<IBaselineProvider>();
+        var diffProvider = new Mock<IDiffProvider>();
+        var contentMatcher = new Mock<IContentMutantMatcher>();
+        var contentTestMatcher = new Mock<IContentTestMatcher>();
+        var options = new StrykerOptions { WithBaseline = true, ProjectVersion = "version" };
+        var source = "class Tests { void Test() { Assert.IsTrue(true); } }";
+        var baselineMutant = new JsonMutant { Status = "Killed", CoveredBy = ["test-id"] };
+        var baselineTestFile = new JsonTestFile
+        {
+            Source = source,
+            Tests = new HashSet<IJsonTest> { new JsonTest("test-id") }
+        };
+        var baseline = new MockJsonReport(null, new Dictionary<string, ISourceFile>
+        {
+            ["foo.cs"] = new MockJsonReportFileComponent("", source, new HashSet<IJsonMutant> { baselineMutant })
+        })
+        {
+            TestFiles = new Dictionary<string, IJsonTestFile> { ["tests.cs"] = baselineTestFile }
+        };
+        var currentTest = CreateCurrentTest(source, "test-id");
+        var currentTestFile = new Mock<ITestFile>();
+        currentTestFile.SetupGet(x => x.RelativePath).Returns("tests.cs");
+        currentTestFile.SetupGet(x => x.Source).Returns(source);
+        currentTestFile.SetupGet(x => x.Tests).Returns(new List<ITestCase> { currentTest });
+        var testProjectsInfo = new Mock<ITestProjectsInfo>();
+        testProjectsInfo.SetupGet(x => x.TestFiles).Returns(new[] { currentTestFile.Object });
+        var gitInfoProvider = new Mock<IGitInfoProvider>();
+        var diff = new DiffResult([], source, source);
+        var file = new CsharpFileLeaf { RelativePath = "foo.cs", SourceCode = source };
+        var mutant = new Mutant { ResultStatus = MutantStatus.Killed, ResultStatusReason = "old result" };
+
+        baselineProvider.Setup(x => x.Load("baseline/version")).ReturnsAsync(baseline);
+        diffProvider.Setup(x => x.GetContentDiff(source, source)).Returns(diff);
+        contentMatcher.Setup(x => x.MatchByLocation(It.IsAny<IEnumerable<IMutant>>(), baselineMutant, diff)).Returns(new[] { mutant });
+        contentTestMatcher.Setup(x => x.IsTestUnchanged(It.IsAny<IJsonTest>(), currentTest, diff)).Returns(false);
+
+        var target = new BaselineMutantFilter(options, baselineProvider.Object, gitInfoProvider.Object,
+            diffProvider.Object, contentMatcher.Object, testProjectsInfo.Object, contentTestMatcher.Object);
+
+        target.FilterMutants(new[] { mutant }, file, options).ToList();
+
+        mutant.ResultStatus.ShouldBe(MutantStatus.Pending);
+        mutant.ResultStatusReason.ShouldBe("One or more covering tests changed since the previous run");
+    }
+
+    [TestMethod]
+    public void FilterMutants_ResetsMutantsToPending_WhenCoveringTestIsMissing()
+    {
+        var baselineProvider = new Mock<IBaselineProvider>();
+        var diffProvider = new Mock<IDiffProvider>();
+        var contentMatcher = new Mock<IContentMutantMatcher>();
+        var options = new StrykerOptions { WithBaseline = true, ProjectVersion = "version" };
+        var source = "class Tests { void Test() { Assert.IsTrue(true); } }";
+        var baselineMutant = new JsonMutant { Status = "Killed", CoveredBy = ["removed-test"] };
+        var baseline = new MockJsonReport(null, new Dictionary<string, ISourceFile>
+        {
+            ["foo.cs"] = new MockJsonReportFileComponent("", source, new HashSet<IJsonMutant> { baselineMutant })
+        });
+        var gitInfoProvider = new Mock<IGitInfoProvider>();
+        var file = new CsharpFileLeaf { RelativePath = "foo.cs", SourceCode = source };
+        var mutant = new Mutant { ResultStatus = MutantStatus.Killed };
+
+        baselineProvider.Setup(x => x.Load("baseline/version")).ReturnsAsync(baseline);
+        contentMatcher.Setup(x => x.MatchByLocation(It.IsAny<IEnumerable<IMutant>>(), baselineMutant, EmptyContentDiff)).Returns(new[] { mutant });
+        diffProvider.Setup(x => x.GetContentDiff(source, source)).Returns(EmptyContentDiff);
+
+        var target = new BaselineMutantFilter(options, baselineProvider.Object, gitInfoProvider.Object,
+            diffProvider.Object, contentMatcher.Object);
+
+        target.FilterMutants(new[] { mutant }, file, options).ToList();
+
+        mutant.ResultStatus.ShouldBe(MutantStatus.Pending);
+        mutant.ResultStatusReason.ShouldBe("One or more covering tests changed since the previous run");
+    }
+
+    private static ITestCase CreateCurrentTest(string source, string id)
+    {
+        var tree = CSharpSyntaxTree.ParseText(source);
+        var node = tree.GetRoot().DescendantNodes().First();
+        var testCase = new Mock<ITestCase>();
+        testCase.SetupGet(x => x.Id).Returns(id);
+        testCase.SetupGet(x => x.Node).Returns(node);
+        return testCase.Object;
     }
 }
